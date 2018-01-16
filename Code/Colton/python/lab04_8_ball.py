83aefb5359178c5b4d68baf6b7388f49204b9a41
adkinswc <adkinswc@gmail.com>
2018-01-16 15:23:39 -0800

    8 Ball Advanced

---
+++ Code/Colton/python/lab04_8_ball.py
@@ -0,0 +1,13 @@
+import random
+print("Welcome to 8 Ball")
+c = ["Yes", "No", "Maybe"]
+answer = ("yes").lower()
+while answer == ("yes"):
+    input("Please type your question below.\n:")
+    print(random.choice(c))
+    answer = input("Try again? Yes or No\n:")
+    if answer == ("no"):
+        break
+    else:
+        print("Let's try that again")
+        answer = ("yes")
