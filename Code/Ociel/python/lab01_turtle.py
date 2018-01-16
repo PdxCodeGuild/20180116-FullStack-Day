e42ded9c85d53ccbbccb53275753e9f785e3d8ad
oglerma <oglerma@gmail.com>
2018-01-16 12:44:51 -0800

    my manly stick figure

---
+++ Code/Ociel/python/lab01_turtle.py
@@ -0,0 +1,69 @@
+"""
+Lab 01: Turtle
+
+"""
+
+from turtle import *
+
+
+#head
+i = 1
+while i < 37:
+    forward(10)
+    left(10)
+    i = i + 1
+
+
+#neck
+right(90)
+forward(20)
+
+
+# right arms
+left(90)
+forward(150)
+
+
+# left arm
+left(180)
+forward(300)
+left(180)
+forward(150)
+right(90)
+
+#body
+forward(100)
+
+
+#right leg
+left(40)
+forward(150)
+left(180)
+forward(150)
+
+
+#left leg
+left(40)
+forward(170)
+left(180)
+forward(170)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+done()
