23f2bdc0ea2c8eecae9819034604dc6b811353e6
oglerma <oglerma@gmail.com>
2018-01-16 15:25:02 -0800

    Added advanced Section and super Advanced sections

--- Code/Ociel/python/lab02_madlib.py
+++ Code/Ociel/python/lab02_madlib.py
@@ -1,3 +1,9 @@
+"""
+Lab 02: Mad Lib
+
+"""
+import random
+
 print("Please provide some info:")
 
 name = input("Name: ")
@@ -10,3 +16,22 @@ sentence = f"I found someone named {name} and he threw some {food} at some pedes
            f"he is an expert in {subject}."
 
 print(sentence)
+
+# Another way to print input string
+
+print("I found someone named {} and he threw some {} at some pedestrians,\n" \
+      "the pedestrians did not like it one bit but my {} was able to rescue me because\n" \
+      "he is an expert in {}.".format(name, food, animal, subject))
+
+# Super Advanced and Advanced combined
+answer = True
+
+while answer != 'no':
+    the_Three_Words = input("Provide three adjectives seperated by commas: ")
+    a, b, c = the_Three_Words.split(",")
+    myList = [a, b, c]
+    print("I found someone named {} and he threw some {} at some pedestrians,\n" \
+          "the pedestrians did not like it one bit but my {} was able to rescue me because\n" \
+          "he is an expert in {}.".format(random.choice(myList), random.choice(myList), random.choice(myList),
+                                          random.choice(myList)))
+    answer = input("Would you like to redo the Mad Lib again?")
