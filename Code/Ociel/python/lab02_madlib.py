0bf3c91ea18dbd2114b49f877f67a58af24b3cc3
oglerma <oglerma@gmail.com>
2018-01-16 14:11:37 -0800

    my own madlib

---
+++ Code/Ociel/python/lab02_madlib.py
@@ -0,0 +1,12 @@
+print("Please provide some info:")
+
+name = input("Name: ")
+food = input("Weird food: ")
+animal = input("Uncommon animal: ")
+subject = input("Subject you hate: ")
+
+sentence = f"I found someone named {name} and he threw some {food} at some pedestrians," \
+           f" the pedestrians did not like it one bit but my {animal} was able to rescue me becuase" \
+           f" he is an expert in {subject}"
+
+print(sentence)
