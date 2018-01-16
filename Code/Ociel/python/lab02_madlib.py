92797c47155fd88ad7f3d84500469db58b1a8178
oglerma <oglerma@gmail.com>
2018-01-16 14:16:43 -0800

    fixed spaces and added new lines

--- Code/Ociel/python/lab02_madlib.py
+++ Code/Ociel/python/lab02_madlib.py
@@ -5,8 +5,8 @@ food = input("Weird food: ")
 animal = input("Uncommon animal: ")
 subject = input("Subject you hate: ")
 
-sentence = f"I found someone named {name} and he threw some {food} at some pedestrians," \
-           f" the pedestrians did not like it one bit but my {animal} was able to rescue me becuase" \
-           f" he is an expert in {subject}"
+sentence = f"I found someone named {name} and he threw some {food} at some pedestrians,\n" \
+           f"the pedestrians did not like it one bit but my {animal} was able to rescue me because\n" \
+           f"he is an expert in {subject}."
 
 print(sentence)
