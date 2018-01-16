f463f181dcf44b492a76044e97d5a2aac3f1f036
michaelx <michaelvertex@gmail.com>
2018-01-16 13:47:56 -0800

    # day one

--- Code/MichaelX/lab01.py
+++ Code/MichaelX/lab01.py
@@ -1,10 +1,46 @@
 from turtle import *
 
+scale = 100
+
+fillcolor('red')
+
+# legs
+left(60)
+forward(100)
+right(120)
+forward(100)
+left(180)
+forward(100)
+left(-30)
+forward(100)
+
+# arms
+left(60)
+forward(100)
+left(180)
+forward(100)
+right(-60)
+forward(100)
+left(180)
+forward(100)
+
+# neck
+left(-120)
+forward(50)
+
+from turtle import *
+
+edge_length = 0
 i = 0
-while i < 4:
-    forward(100)
-    left(90)
-    i = i + 1
-    print(i)
+while i < 100:
+	forward(edge_length)
+	right(144)
+
+	edge_length += 4
+
+done()
+
+
+end_fill()
 
 done()
