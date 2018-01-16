b543c5f895c564bda67131c07b70e229ff2fd628
adkinswc <adkinswc@gmail.com>
2018-01-16 15:28:39 -0800

    Emoji Generator

--- Code/Colton/python/lab05_Emoji.py
+++ Code/Colton/python/lab05_Emoji.py
@@ -2,3 +2,7 @@ import random
 eyes = [':', ';', '8']; nose = ['-', '~', 'o']; mouth =['P', 'D', ')', '(']
 print(random.choice(eyes) + random.choice(nose) + random.choice(mouth))
 
+count = 0
+while(count < 4):
+    print(random.choice(eyes) + random.choice(nose) + random.choice(mouth))
+    count = count + 1
