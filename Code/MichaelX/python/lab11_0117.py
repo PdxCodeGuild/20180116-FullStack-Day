6986f94cbce3fb9bb9acc546df9bc4a34880964c
michaelx <michaelvertex@gmail.com>
2018-01-17 15:35:15 -0800

    # day one

--- Code/MichaelX/python/lab11_0117.py
+++ Code/MichaelX/python/lab11_0117.py
@@ -1,6 +1,8 @@
-operator = input('What is the operation you\'d like to perform?')
+# operator = input('What is the operation you\'d like to perform?')
 
 first = input('What is the first number?')
 seccond = input('What is the second number?')
 
-print(f'{first}+{operator}+{second}')
+sum = first+second
+
+print(sum)
---
+++ Code/MichaelX/python/lab12_0117.py
@@ -0,0 +1,15 @@
+import random
+
+x = random.randint(1, 10)
+
+guess = input('> guess the number? \n >')
+
+while guess < 10:
+    if guess == x:
+        print('correct')
+    elif guess == x:
+        print('correct')
+
+
+print(x)
+
