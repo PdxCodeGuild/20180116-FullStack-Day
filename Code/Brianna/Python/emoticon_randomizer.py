b3dcb25f0008178a411f16bc5b0610550b4e5820
Brianna Harris <Brianna@BriannaHarris.net>
2018-01-16 16:16:32 -0800

    Emoticon randomizer using while loop to print 5 times.

--- Code/Brianna/Python/emoticon_randomizer.py
+++ Code/Brianna/Python/emoticon_randomizer.py
@@ -6,5 +6,8 @@ mouth_list = [")", "(", "}", "{", "D", "P"]
 
 emotiocon = random.choice(eye_list) + random.choice(nose_list) + random.choice(mouth_list)
 
-print(emotiocon)
+run_emotions = 0
 
+while run_emotions < 5:
+    print(emotiocon)
+    run_emotions = run_emotions + 1
