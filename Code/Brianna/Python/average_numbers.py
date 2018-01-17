a1d494678bce8f6b3ff28c5a39c1a7f384d0a66b
Brianna Harris <Brianna@BriannaHarris.net>
2018-01-17 15:54:40 -0800

    Average numbers

---
+++ Code/Brianna/Python/average_numbers.py
@@ -0,0 +1,13 @@
+
+# We're going to average a list of numbers. Start with the following list, iterate through it, keeping a 'running sum', then divide that sum by the number of elements in that list. Remember len will give you the length of a list.
+# The code below hows how to loop through an array, and prints the elements one at a time.
+
+
+nums = [5, 0, 8, 3, 4, 1, 6]
+
+number_sum = sum(nums)
+list_length = len(nums)
+print(number_sum // list_length)
+
+
+
