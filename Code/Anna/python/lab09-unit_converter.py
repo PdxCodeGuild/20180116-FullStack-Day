1b7a3c7a00f508561e755b495d166af2a589c088
Anna <anna@annaspysz.com>
2018-01-17 13:46:00 -0800

    Improved the while loop, added validation

--- Code/Anna/python/lab09-unit_converter.py
+++ Code/Anna/python/lab09-unit_converter.py
@@ -4,21 +4,18 @@ Let's convert some units!
 """
 
 print("Welcome to the unit converter! The acceptable units are in, ft, yd, mi, cm, m, and km.\n")
-number = float(input("Enter your length: "))
-input_unit = input("Enter the original unit: ")
-output_unit = input("Enter the unit you would like to convert to: ")
 
-meter_conversion_dict = {'in': 39.37008, 'ft': 3.28084, 'yd': 1.09361, 'mi': 0.000621371, 'cm': 100, 'm': 1, 'km': 0.001}
-def convert_units(number, input_unit, output_unit):
-    meters = number / meter_conversion_dict[input_unit]
-    return meters * meter_conversion_dict[output_unit]
-
-print(str(number) + input_unit + " is " + str(convert_units(number, input_unit, output_unit)) + output_unit + "\n")
-
-cont = input("Would you like to convert another unit? y/n: ")
+cont = 'y'
 
 while cont == 'y':
-    number = float(input("Enter your length: "))
+    while True:
+        number = input("Enter your length: ")
+        try:
+            number = float(number)
+            break
+        except ValueError:
+            print("Please enter a number.")
+
     input_unit = input("Enter the original unit: ")
     output_unit = input("Enter the unit you would like to convert to: ")
 
@@ -27,7 +24,8 @@ while cont == 'y':
         meters = number / meter_conversion_dict[input_unit]
         return meters * meter_conversion_dict[output_unit]
 
-    print(str(number) + input_unit + " is " + str(convert_units(number, input_unit, output_unit)) + output_unit + "\n")
+    result = convert_units(number, input_unit, output_unit)
+    print(f"{number} {input_unit} is {result} {output_unit}\n")
 
     cont = input("Would you like to convert another unit? y/n: ")
 
