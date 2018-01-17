87235bb01f0dd201928d48f9c2d617689b83504c
Brianna Harris <Brianna@BriannaHarris.net>
2018-01-16 16:17:24 -0800

    Emoticon randomizer using while loop to print 5 times with 5 different faces.

--- Code/Brianna/Python/emoticon_randomizer.py
+++ Code/Brianna/Python/emoticon_randomizer.py
@@ -4,10 +4,10 @@ eye_list = [":", ";", "=", "8"]
 nose_list = ["-", "o", "*"]
 mouth_list = [")", "(", "}", "{", "D", "P"]
 
-emotiocon = random.choice(eye_list) + random.choice(nose_list) + random.choice(mouth_list)
 
 run_emotions = 0
 
 while run_emotions < 5:
+    emotiocon = random.choice(eye_list) + random.choice(nose_list) + random.choice(mouth_list)
     print(emotiocon)
     run_emotions = run_emotions + 1
