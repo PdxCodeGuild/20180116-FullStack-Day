b6b49b040410a2f80e48b22e1bffa77bf1c95217
Brianna Harris <Brianna@BriannaHarris.net>
2018-01-17 13:54:05 -0800

    Mostly accurate change converter. Something is odd with pennies...

---
+++ Code/Brianna/Python/Unit_converter.py
@@ -0,0 +1,23 @@
+#Make a dictionary for unit values
+
+units_in_meter = {'meter': 1.0, 'foot': 0.3048, 'mile': 1609.34, 'kilometer': 1000, 'm': 1.0, 'ft': 0.3048, 'mi': 1609.34, 'km': 1000 }
+
+# Define the units to be input and out put
+input_unit = int(input("What unit of measurement would you like to convert from?\n:"))
+output_unit = int(input("What unit of measurement would you like to convert from?\n:"))
+
+# Define how many to convert
+number_of_units = int(input("How many units would you like to convert?\n:"))
+
+# Calculate units to maths
+input_number = units_in_meter['input_unit']
+output_number = units_in_meter['output_unit']
+
+# Calculate conversion
+if input_unit != 'meter' or input_unit != 'm'
+
+
+
+# Print
+
+print("There are " + str()  + " " + str(output_unit) + " in " + str() + " " + str(input_unit) + ".")
