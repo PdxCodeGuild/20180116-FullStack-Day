0f25d0b0bf0c1e024d4f554d79c23384b2017983
michaelx <michaelvertex@gmail.com>
2018-01-16 16:49:15 -0800

    # day one

---
+++ Code/MichaelX/lab03_0116.py
@@ -0,0 +1,19 @@
+
+
+
+grade = int(input('What? '))
+
+if grade >= 90:
+    print('You got a A')
+
+elif grade >= 80 and grade <= 89:
+    print('You got a B')
+
+elif grade > 70 and grade < 79:
+    print('You got a C')
+
+elif grade > 60 and i < 69:
+    print('You got a D')
+
+else:
+    print('You failed')
