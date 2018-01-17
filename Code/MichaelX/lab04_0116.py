ad1a4ecd949ad0bed3441857723bd1996854e30b
michaelx <michaelvertex@gmail.com>
2018-01-16 21:40:48 -0800

    # day one

--- Code/MichaelX/lab04_0116.py
+++ Code/MichaelX/lab04_0116.py
@@ -2,11 +2,11 @@ import random
 
 def eightb():
 
-    magic_ans = ['It is certain', 'It is decidedly so ', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes',  'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+    ans = ['It is certain', 'It is decidedly so ', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes',  'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
 
     magic = input('> Ask a question? ')
 
-    magicks = random.choice(magic_ans)
+    magicks = random.choice(ans)
 
     print(f'Q: {magic} A: {magicks}')
 
