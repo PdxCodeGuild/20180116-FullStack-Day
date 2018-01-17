eb17efe3450c26195af9539619f6a677fa10d203
Nikko Geise <nikko@geise.com>
2018-01-17 09:30:03 -0800

    updates

--- Code/Maggie/l05_emoji.py
+++ Code/Maggie/l05_emoji.py
@@ -6,7 +6,7 @@ MOUTHS = [')', 'D', '(', '[', ']', '/', 'P', 'p']
 
 def emoji():
     i = 0
-    print('The notorious gang of five: \n')
+    print('Emoji Generator. \nHere are your five faces:\n')
     while i < 5:
         print(random.choice(EYES) + random.choice(NOSES) + random.choice(MOUTHS) + '\n')
         i += 1
