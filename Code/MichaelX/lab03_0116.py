9cdf08f86fadb00ab4f3bbdfd9354f9a97d333fb
michaelx <michaelvertex@gmail.com>
2018-01-16 21:23:41 -0800

    # day one

--- Code/MichaelX/lab03_0116.py
+++ Code/MichaelX/lab03_0116.py
@@ -1,19 +1,22 @@
 
 
+def school():
 
-grade = int(input('What? '))
+    grade = int(input('What percentage did you get on your test? '))
 
-if grade >= 90:
-    print('You got a A')
+    if grade >= 90:
+        print('You got a A')
 
-elif grade >= 80 and grade <= 89:
-    print('You got a B')
+    elif grade >= 80 and grade <= 89:
+        print('You got a B')
 
-elif grade > 70 and grade < 79:
-    print('You got a C')
+    elif grade >= 70 and grade < 79:
+        print('You got a C')
 
-elif grade > 60 and i < 69:
-    print('You got a D')
+    elif grade >= 60 and i < 69:
+        print('You got a D')
 
-else:
-    print('You failed')
+    else:
+        print('You failed')
+
+school()
