e3dfb2cbec165a1b308384edb38bf9f5ca504030
michaelx <michaelvertex@gmail.com>
2018-01-17 22:47:39 -0800

    # day one

--- Code/Michael/python/lab07_0117.py
+++ Code/Michael/python/lab07_0117.py
@@ -1,32 +1,38 @@
 from random import randint
 
-t = ["Rock", "Paper", "Scissors"]
+t = ['rock', 'paper', 'scissors']
 
-computer = t[randint(0,2)]
+computer = t[randint(0, 2)]
 
 player = False
 
 while player == False:
-    player = input("Rock, Paper, Scissors?")
+
+    player = input('Rock, paper or scissors? \n> ')
+
     if player == computer:
-        print("Tie!")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer, "covers", player)
+        print('Its a Tie!')
+
+    elif player == 'rock':
+        if computer == 'paper':
+            print('You lose,', computer, 'covers', player)
         else:
-            print("You win!", player, "smashes", computer)
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer, "cut", player)
+            print('You win,', player, 'smashes', computer)
+
+    elif player == 'paper':
+        if computer == 'scissors':
+            print('You lose,', computer, 'cuts', player)
         else:
-            print("You win!", player, "covers", computer)
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("You lose...", computer, "smashes", player)
+            print('You win,', player, 'covers', computer)
+
+    elif player == 'scissors':
+        if computer == 'rock':
+            print('You lose,', computer, 'smashes', player)
         else:
-            print("You win!", player, "cut", computer)
+            print('You win,', player, 'cuts', computer)
+
     else:
-        print("That's not a valid play. Check your spelling!")
+        print('You must have entered something wrong. Please, try again!')
 
     player = False
-    computer = t[randint(0,2)]
+    computer = t[randint(0, 2)]
