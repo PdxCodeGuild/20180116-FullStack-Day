2bf93c73d757721b075dfe75d6bd7fd4957f04e5
sprread <sprread@gmail.com>
2018-01-17 16:34:46 -0800

    Finished all versions

---
+++ Code/Jackson/Python/Lab010_AverageNumbers.py
@@ -0,0 +1,33 @@
+"""
+We're going to average a list of numbers. Start with the following list, iterate through it, keeping a 'running sum',
+then divide that sum by the number of elements in that list. Remember len will give you the length of a list.
+"""
+
+#Ask the user for numbers and append each one to the list
+
+nums = []
+newnumber = 0
+
+newnumber = (input('enter a number to the list and I will average them! (type "done" to get average): '))
+
+while newnumber != ('done'):
+    nums.append(newnumber)
+    newnumber = (input('enter a number to the list and I will average them! (type "done" to get average): '))
+
+
+
+#for loop that goes through the array and sums the numbers
+
+sum = 0
+for num in nums:
+    sum += int(num)
+
+
+#average the numbers
+
+avg = sum / len(nums)
+
+
+#print the average
+
+print('the average is ' + str(avg))
