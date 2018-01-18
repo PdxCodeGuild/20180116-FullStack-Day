121f6170021cc7077e4a492ca552cf5a4864dec3
Anna <anna@annaspysz.com>
2018-01-17 16:41:41 -0800

    Added version 5

--- Code/Anna/python/lab12-guess_the_number.py
+++ Code/Anna/python/lab12-guess_the_number.py
@@ -12,7 +12,7 @@ print("Welcome to the guessing game! You have 10 guesses.")
 computer_guess = random.randint(1, 10)
 
 game_on = True
-number_tries = 10
+number_tries = 9
 
 while game_on is True and number_tries > 0:
     user_guess = input("Guess a number between 1 and 10.\n> ")
@@ -22,7 +22,7 @@ while game_on is True and number_tries > 0:
         game_on = False
     else:
         number_tries -= 1
-        print("Wrong! You have " + str(number_tries) + " tries left.\n")
+        print("Wrong! You have " + str(number_tries + 1) + " tries left.\n")
 
 print("Game over.\n")
 
@@ -66,5 +66,27 @@ while True:
             print("Wrong! Too high!\n")
         else:
             print("Wrong! Too low!\n")
-print("Game over.")
+print("Game over.\n")
 
+# version 4
+
+
+
+# version 5
+
+number_tries = 1
+
+user_guess = int(input("Enter a number between 1 and 10 for the computer to guess\n:"))
+
+while computer_guess != user_guess: # and number_tries < 100:
+    computer_guess = random.randint(1, 10)
+    if computer_guess == user_guess:
+        print(computer_guess)
+        print(f"The computer got it in just {number_tries} guesses!")
+        break
+    else:
+        print(computer_guess)
+        number_tries += 1
+        continue
+
+print("Game over, human.")
