a6d5e28d2198830b6ea14aae5ec7da226dc2c899
michaelx <michaelvertex@gmail.com>
2018-01-17 23:04:31 -0800

    # day one

--- Code/MichaelX/python/lab11_0117.py
+++
@@ -1,8 +0,0 @@
-# operator = input('What is the operation you\'d like to perform?')
-
-first = input('What is the first number?')
-seccond = input('What is the second number?')
-
-sum = first +second
-
-print(sum)
