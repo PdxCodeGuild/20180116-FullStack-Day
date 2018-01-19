27d4138f0863d9152ab095852dc6fb2446dd328c
Brianna Harris <Brianna@BriannaHarris.net>
2018-01-19 12:28:06 -0800

    Cypher with rotation selection.

---
+++ Code/Brianna/Python/Rot_13.py
@@ -0,0 +1,12 @@
+import string
+
+input_phrase = input("Please type your word to by encrypted in Upper Case letters with no spaces or additional characters.\n:")
+new_phrase = ''
+alphabet_list = list(string.ascii_uppercase)
+rotation = input("By how much would you like to rotate your cypher? Please enter a number.\n:")
+
+for i in range(len(input_phrase)):
+    new_phrase = new_phrase + alphabet_list[(((alphabet_list.index(input_phrase[i].upper())) + rotation)% 26)]
+
+
+print(new_phrase)
