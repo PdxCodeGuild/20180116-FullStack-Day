34dd63b9272b081b5c3ae73973ebcc6c0a0ce110
Anna <anna@annaspysz.com>
2018-01-19 15:29:45 -0800

    Added version 3

--- Code/Anna/python/lab16-image_manipulation.py
+++ Code/Anna/python/lab16-image_manipulation.py
@@ -111,3 +111,33 @@ else:
     else:
         print("OK, your loss. Let's move on.")
 
+# version 3
+
+width = 500
+height = 500
+
+img = Image.new('RGB', (width, height))
+
+draw = ImageDraw.Draw(img)
+
+
+# the origin (0, 0) is at the top-left corner
+
+draw.rectangle(((0, 0), (width, height)), fill="white")
+
+# draw a rectangle from x0, y0 to x1, y1
+draw.rectangle(((100, 100), (300, 300)), fill="lightblue")
+
+# draw a line from x0, y0, x1, y1
+# using the color pink
+color = (256, 128, 128)  # pink
+draw.line((0, 0, width, height), fill=color)
+draw.line((0, height, width, 0), fill=color)
+
+
+circle_x = width/2
+circle_y = height/2
+circle_radius = 100
+draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='lightgreen')
+
+img.show()
