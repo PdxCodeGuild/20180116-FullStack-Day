2f5842c387387613f9bbe456142d47b6b61814e6
MattB <mbeck410@gmail.com>
2018-01-19 15:05:18 -0800

    Changed to inputs to palindrome.

--- Code/MattB/python/lab17_pal_ana.py
+++ Code/MattB/python/lab17_pal_ana.py
@@ -1,10 +1,10 @@
-print('Let\'s check if a word is an anagram.')
-anagram = input('Type in a word: ')
+print('Let\'s check if a word is an palindrome.')
+palin = input('Type in a word: ')
 
-for i in range(0, len(anagram)):
-    if anagram[i] != anagram[len(anagram) - 1 - i]:
-        print('This word is not an anagram')
+for i in range(0, len(palin)):
+    if palin[i] != palin[len(palin) - 1 - i]:
+        print('This is not a palindrome.')
         break
-    elif i == len(anagram) - 1:
-        print('This is an anagram')
+    elif i == len(palin) - 1:
+        print('This is a palindrome.')
 
