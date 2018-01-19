a39761f27f687cd0405a0a7622b7646371b45918
michaelx <michaelvertex@gmail.com>
2018-01-18 21:25:47 -0800

    # day one

---
+++ Code/Michael/python/lab14_0118.py
@@ -0,0 +1,20 @@
+# from random
+
+# numb = sorted(random.sample(range(1,48), 6))
+# print(numb)
+
+import random
+
+'''i = 0
+
+while i < 6:
+    i += 1
+    nums = random.randint(1, 48)
+#    numb = nums.sort()
+    print(nums)'''
+
+
+deck = list(range(1, 48))
+nums.pop() = random.shuffle(deck)
+
+print(nums)
