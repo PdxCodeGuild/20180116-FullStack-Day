009a56d1373997ed2871743680cf52994014df84
michaelx <michaelvertex@gmail.com>
2018-01-19 16:37:07 -0800

    # day one

--- Code/Michael/python/lab13b_0118.py
+++ Code/Michael/python/lab13b_0118.py
@@ -1,22 +1,22 @@
-#
-#
-# def rot(x):
-#
-#     x = int(input('> Type the rotation amount. \n> '))
-#
-#     instring = input('> Type something. \n> ')
-#
-#     result = ""
-#
-#     for i in instring:
-#         chars = ord(i)
-#         if chars >= ord('a') and chars <= ord('z'):
-#             if chars > ord('m'):
-#                 chars -= x
-#             else:
-#                 chars += x
-#         result += chr(chars)
-#
-#     print(result)
-#
-# rot(13)
+
+
+def rot(x):
+
+    x = int(input('> Type the rotation amount. \n> '))
+
+    instring = input('> Type something. \n> ')
+
+    result = ""
+
+    for i in instring:
+        chars = ord(i)
+        if chars >= ord('a') and chars <= ord('z'):
+            if chars > ord('m'):
+                chars -= x
+            else:
+                chars += x
+        result += chr(chars)
+
+    print(result)
+
+rot(13)
