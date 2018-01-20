bbd77f1fbc9fef44992412a017548489ea52cf76
Brianna Harris <Brianna@BriannaHarris.net>
2018-01-19 16:28:13 -0800

    Claims r is not defined. A mystery for later.

--- Code/Brianna/Python/Image Manipulation 2 Saturation.py
+++ Code/Brianna/Python/Image Manipulation 2 Saturation.py
@@ -5,33 +5,22 @@ img = Image.open("lenna.png") # must be in same folder
 width, height = img.size
 pixels = img.load()
 
-for i in range(width):
-    for j in range(height):
-        #defining all those pixels
-        r, g, b = pixels[i, j]
-        y = 0.299 * r + 0.587 * g + 0.114 * b
-        int(y)
-        # redefining all those pixels by switching stuff about and replacing it with y.
-        pixels[i, j] = (int(y), int(y), int(y))
 
-img.show()
 # This math does not work when applied below. :-)
 for i in range(width):
     for j in range(height):
         h, s, v = colorsys.rgb_to_hsv(r / 255, g / 255, b / 255)
-        saturated = 0.7 * h + 0.4 * s + 0.3 * v
-        int(saturated)
-        r, g, b = colorsys.hsv_to_rgb(saturated/255, saturated/255, saturated/255)
+
+        h = h * .7
+        s = s * .7
+        v = v * .7
+
+pixels[i, j] = (r, g, b)
 
 img.show()
 
 # colorsys uses colors in the range [0, 1]
 
-
-# do some math on h, s, v
-saturated =
-
-r, g, b = colorsys.hsv_to_rgb(h, s, v)
 
 # convert back to [0, 255]
 
