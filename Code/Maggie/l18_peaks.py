472f967a4e93124e16646a06b7a36b73ccf2d0d8
Nikko Geise <nikko@geise.com>
2018-01-22 12:50:11 -0800

    fixed Pep8

--- Code/Maggie/l18_peaks.py
+++ Code/Maggie/l18_peaks.py
@@ -4,33 +4,35 @@ data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]  # the da
 
 
 # if the value at the previous and post index is lower/higher, index marks a peak/valley
-def peaks(list):
+def peaks(lst):
     pk_i = []
-    for i in range(1,len(list)-1):
-       if list[i] > list[i - 1] and list[i] > list[i + 1]:
+    for i in range(1, len(lst)-1):
+        if lst[i] > lst[i - 1] and lst[i] > lst[i + 1]:
             pk_i.append(i)
     return pk_i
 
-def valleys(list):
+
+def valleys(lst):
     v_i = []
-    for i in range(1,len(list)-1):
-        if list[i] < list[i - 1] and list[i] < list[i + 1]:
+    for i in range(1, len(lst)-1):
+        if lst[i] < lst[i - 1] and lst[i] < lst[i + 1]:
             v_i.append(i)
     return v_i
 
 
-def peaks_and_valleys(list):
+def peaks_and_valleys(lst):
     pv_i = []
-    for i in range(1,len(list)-1):
-        if list[i] < list[i - 1] and list[i] < list[i + 1]:
+    for i in range(1, len(lst)-1):
+        if lst[i] < lst[i - 1] and lst[i] < lst[i + 1]:
             pv_i.append(i)
-        elif list[i] > list[i - 1] and list[i] > list[i + 1]:
+        elif lst[i] > lst[i - 1] and lst[i] > lst[i + 1]:
             pv_i.append(i)
     return pv_i
 
+
 def drawlist(lst):
     graph = ''
-    for line in range(max(lst)):# start with line 1, iterate through each line
+    for line in range(max(lst)):  # start with line 1, iterate through each line
         for i in range(len(lst)):  # iterate through each space (x value) in the line
             if lst[i] >= (max(lst) - line):  # the first line should be the max, second line = max -1
                 graph += ' x '
@@ -40,14 +42,12 @@ def drawlist(lst):
             graph += '\n'  # skip a line at the end of each row, except for last line
     print(graph)
     print(data)
-    print('\n')
 
-# v3: while iterating through each item, if you hit a max point, next lines will be 0 til hitting it again.
-# peaks will give you these indexes.. if you hit a max
 
 def draw_filled(lst):  # uses a toggle to switch from space to fill
     graph = ''
     fill = False
+    water = 0
     for line in range(max(lst)):  # start with line 1, iterate through each line
         for i in range(len(lst)):  # iterate through each space (x value) in the line
             if lst[i] >= (max(lst) - line):  # the first line should be the max, second line = max -1
@@ -56,6 +56,7 @@ def draw_filled(lst):  # uses a toggle to switch from space to fill
             else:
                 if fill:
                     graph += ' 0 '
+                    water += 1
                 else:
                     graph += '   '
         if line != max(lst) - 1:
@@ -63,9 +64,11 @@ def draw_filled(lst):  # uses a toggle to switch from space to fill
         fill = False
     print(graph)
     print(data)
+    print('amount of water collected: ', water, 'units')
 
-print(peaks(data))
-print(valleys(data))
-print(peaks_and_valleys(data))
+
 drawlist(data)
+print('Peaks at index: ', peaks(data))
+print('Valleys at index: ', valleys(data))
+print('Combined peaks and valleys: ', peaks_and_valleys(data), '\n')
 draw_filled(data)
