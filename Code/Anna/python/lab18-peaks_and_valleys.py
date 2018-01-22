e02793b132dc7a1881a47d03ad067c7b12e58f56
Anna <anna@annaspysz.com>
2018-01-22 12:10:43 -0800

    Added version 2

--- Code/Anna/python/lab18-peaks_and_valleys.py
+++ Code/Anna/python/lab18-peaks_and_valleys.py
@@ -2,11 +2,21 @@
 Lab 18
 Peaks and valleys
 """
+import random
 
 data_list = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 peak_list = []
 valley_list = []
 both_list = []
+x_list = []
+rand_x_list = []
+xes_list = []
+rand_xes_list = []
+
+rand_data_list = []
+
+for i in range(0, 100):
+    rand_data_list.append(random.randint(1, 9))
 
 
 def peaks(data):
@@ -27,8 +37,8 @@ def valleys(data):
 
 def peaks_and_valleys():
     # return both
-    both_list = peak_list + valley_list
-    return both_list
+    both = peak_list + valley_list
+    return both
 
 
 peaks(data_list)
@@ -37,3 +47,29 @@ valleys(data_list)
 print(f"All the peaks: {peak_list}")
 print(f"All the valleys: {valley_list}")
 print(f"This list has both! {peaks_and_valleys()}")
+
+
+# Version 2
+
+print("\nNow let's make it pretty!")
+# make it visual
+
+max_number = max(data_list)
+
+for i in range(0, max_number):
+    for x in range(0, len(data_list)):
+        if data_list[x] >= max_number:
+            x_list.append(" X ")
+        else:
+            x_list.append("   ")
+    max_number -= 1
+    xes_list.append(x_list)
+    x_list = []
+
+
+for l in xes_list:
+    print(''.join(l))
+
+
+
+
