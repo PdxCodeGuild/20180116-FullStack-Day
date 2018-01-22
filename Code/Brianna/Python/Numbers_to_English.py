b16c993993e70c1a24dae3bab4999361b55ea551
Brianna Harris <Brianna@BriannaHarris.net>
2018-01-22 11:50:31 -0800

    Committing all files in my Python folder

--- Code/Brianna/Python/Numbers_to_English.py
+++ Code/Brianna/Python/Numbers_to_English.py
@@ -14,7 +14,6 @@ singles_list = {0 : 'zero', 1 : 'one',  2 : 'two', 3 : 'three', 4 : 'four', 5 : 
 teens_list = {11 : 'eleven', 12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen', 15 : 'fifteen', 16 : 'sixteen', 17 : 'seventeen', 18 : 'eighteen', 19 : 'nineteen' }
 hundreds_list = {1 : 'one hundred', 2: 'two hundred', 3 : 'three hundred', 4 : 'four hundred', 5 : 'five hundred', 6 : 'six hundred', 7 : 'seven hundred', 8 : 'eight hundred', 9 : 'nine hundred' }
 
-
 # Check if number is over 100
 
 if input_number >= 100 and input_number:
@@ -29,6 +28,7 @@ if input_number >= 100 and input_number:
         print(f'{hundreds} and {tens_list[1]}.')
    else:
         print(f'{hundreds} and {tens_list[new_tens] + dash + singles_list[new_ones]}')
+
 
 elif input_number < 20 and input_number > 10:
     print(teens_list[input_number])
