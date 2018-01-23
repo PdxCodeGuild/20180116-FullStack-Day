b3ca9c883d98523a26d4ad2c5707cbaf3adcb25c
sprread <sprread@gmail.com>
2018-01-22 20:09:29 -0800

    done

--- Code/Jackson/Python/Lab005_RandomEmoticonGenerator.py
+++ Code/Jackson/Python/Lab005_RandomEmoticonGenerator.py
@@ -5,40 +5,27 @@ Random Emoticon Generator
 import random
 
 #Define a list of eyes
-
 eyes = [':', ';', '8']
 
 #Define a list of noses
-
 noses = ['^', '', '~']
 
 #Define a list of mouths
-
 mouths = ['p', '\\', '{']
 
 #Randomly pick a set of eyes
-
 eye = random.choice(eyes)
 
 #Randomly pick a nose
-
 nose = random.choice(noses)
 
 #Randomly pick a mouth
-
 mouth = random.choice(mouths)
-
-#Assemble and display the emoticon
-
-print(eye+nose+mouth)
 
 #use a while loop to produce 5 emoticons
 
-x = 0
-while x < 4: #4 more
-    eye = random.choice(eyes)
-    nose = random.choice(noses)
-    mouth = random.choice(mouths)
-    print(eye + nose + mouth)
-    x += 1
+eye = random.choice(eyes)
+nose = random.choice(noses)
+mouth = random.choice(mouths)
+print(eye + nose + mouth)
 
