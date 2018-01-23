fc7077be09a5652d484ec2cb85af70d23ec72e92
Brianna Harris <Brianna@BriannaHarris.net>
2018-01-23 10:15:56 -0800

    Pick six done

--- Code/Brianna/Python/Pick_6.py
+++ Code/Brianna/Python/Pick_6.py
@@ -9,47 +9,30 @@ def pick6():
     return lottery
 
 # Make a comparison function
-def compare(primary_lottery, human_ticket)
+def compare(primary_lottery, human_ticket):
     match = 0
     for i in range(len(primary_lottery)):
         if primary_lottery[i] == human_ticket[i]:
-            match += 1
+            match = match + 1
     return match
 
-
-# Make primary lottery ticket user is trying to win against.
-
-primary_lottery = pick6()
-print("The winning ticket is: " + primary_lottery)
-
-
-# Make a lottery ticket for the human
-
-human_ticket = pick6()
-print("Your ticket was " + human_ticket)
-
 balance = 0
+game = 0
 
 
+lottery_winnings = {0 : 0, 1 : 4, 2 : 7, 3 : 100, 4 : 50000, 5 : 1000000, 6 : 25000000}
 
-'''
-a ticket costs $2
-if 1 number matches, you win $4
-if 2 numbers match, you win $7
-if 3 numbers match, you win $100
-if 4 numbers match, you win $50,000
-if 5 numbers match, you win $1,000,000
-if 6 numbers match, you win $25,000,000
-One function you might write is pick6() which will generate a list of 6 random numbers, which can then be used for both the winning numbers and tickets. Another function could be num_matches(winning, ticket) which returns the number of matches between the winning numbers and the ticket.
+while game < 200000:
+    primary_lottery = pick6()
+    human_ticket = pick6()
+    balance = balance -2
+    match = compare(primary_lottery,human_ticket)
+    #print('match',match)
+    balance = balance + lottery_winnings[match]
+    #print('balance',balance)
+    game = game + 1
+    #print('game',game)
 
-Steps
-Generate a list of 6 random numbers representing the winning tickets
-Start your balance at 0
-Loop 100,000 times, for each loop:
-Generate a list of 6 random numbers representing the ticket
-Subtract 2 from your balance (you bought a ticket)
-Find how many numbers match
-Add to your balance the winnings from your matches
-After the loop, print the final balance
-
-'''
+print("You monnies are now....", balance, "!")
+if balance < 0:
+    print("Ouch! That hurt!")
