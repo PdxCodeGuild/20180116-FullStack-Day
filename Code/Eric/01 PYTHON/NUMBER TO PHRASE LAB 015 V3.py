94fd836d57d3a9ce1b9c3931072b7c3b28945ad2
efeezyubeezy <e.feezius@gmail.com>
2018-01-24 10:38:01 -0800

    WIP

--- Code/Eric/01 PYTHON/NUMBER TO PHRASE LAB 015 V3.py
+++ Code/Eric/01 PYTHON/NUMBER TO PHRASE LAB 015 V3.py
@@ -1,4 +1,6 @@
 # VERSION 3: FEATURING THE ROMANS!!!
+# WORK IN PROGRESS!!!
+
 
 # number_dict = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight',
 #                9: 'Nine', 10: 'Ten', 11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen',
