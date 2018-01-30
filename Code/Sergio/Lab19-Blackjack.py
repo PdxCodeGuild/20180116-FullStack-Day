51158091a197d1452a33f1b7617b0dffcd8e5288
b1oo <sergiobluez@gmail.com>
2018-01-29 19:25:17 -0800

    Blackjack Advice.

--- Code/Sergio/Lab19-Blackjack.py
+++ Code/Sergio/Lab19-Blackjack.py
@@ -4,17 +4,18 @@
 # A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K.
 card_values = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10}
 
-card_sum = 0
-
+# Ask user for their first 3 cards
 user_card1 = input('What\'s your first card? ')
 user_card2 = input('What\'s your second card? ')
 user_card3 = input('What\'s your third card? ')
 
-
+# Sums up cards
+card_sum = 0
 card_sum = card_values[user_card1] + card_values[user_card2] + card_values[user_card3]
 print(card_sum)
 
-def play(card_values):
+# Card Advice
+def play():
 
     if card_sum < 17:
         print('Hit')
@@ -25,4 +26,4 @@ def play(card_values):
     elif card_sum >= 22:
         print('Already busted')
 
-play(card_values)
+play()
