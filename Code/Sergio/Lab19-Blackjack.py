1f84a39d3abcbebc330af7d329290f35976d6629
b1oo <sergiobluez@gmail.com>
2018-01-29 19:22:40 -0800

    Blackjack Advice.

---
+++ Code/Sergio/Lab19-Blackjack.py
@@ -0,0 +1,28 @@
+"Lab 19: Blackjack Advice"
+
+# Figure out point value of each card individually
+# A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K.
+card_values = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10}
+
+card_sum = 0
+
+user_card1 = input('What\'s your first card? ')
+user_card2 = input('What\'s your second card? ')
+user_card3 = input('What\'s your third card? ')
+
+
+card_sum = card_values[user_card1] + card_values[user_card2] + card_values[user_card3]
+print(card_sum)
+
+def play(card_values):
+
+    if card_sum < 17:
+        print('Hit')
+    elif card_sum <= 17:
+        print('Stay')
+    elif card_sum == 21:
+        print('Blackjack!')
+    elif card_sum >= 22:
+        print('Already busted')
+
+play(card_values)
