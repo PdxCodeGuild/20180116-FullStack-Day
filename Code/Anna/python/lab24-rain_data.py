a247618deeeac51800584e8eecfce132a8b03f51
Anna <anna@annaspysz.com>
2018-01-30 11:08:25 -0800

    Added version 3 - fun with Pyplot!

--- Code/Anna/python/lab24-rain_data.py
+++ Code/Anna/python/lab24-rain_data.py
@@ -6,15 +6,16 @@ But it never rains in Portland...
 import requests
 import datetime
 import re
+import matplotlib.pyplot as plt
 
 r = requests.get('https://or.water.usgs.gov/non-usgs/bes/mt_tabor.rain')    # because text files are for chumps
 data = r.text.split('\n')       # listify
 
-for i in range(0, 12):          # removing comments in the beginning
+for i in range(0, 12):          # removing comments in the beginning, current day b/c incomplete
     data.remove(data[0])
 data.pop()                      # removing last blank line
 
-split_data = []
+split_data = []                 # splitting further into dates and numbers
 for i in range(len(data)):
     split_data.append(re.split('\s+', data[i]))
 
@@ -26,6 +27,7 @@ for i in range(len(split_data)):
     # print(date.month)
     # print(date.day)
 
+
 # Version 2
 
 sum_list = []
@@ -35,7 +37,6 @@ for i in range(len(split_data)):
     except ValueError:
         sum_list.append(0)
 
-print(sum_list)
 
 total = len(sum_list)
 sums = 0
@@ -60,7 +61,7 @@ def get_var(mean):                                 # get the variance
 mean = get_mean(total, sums)
 variance = get_var(mean)
 
-print(f"The mean of the data is: {round(mean, 2)}")
+print(f"\nThe mean of the data is: {round(mean, 2)}")
 print(f"The variance of the data is: {round(variance, 2)}")
 
 
@@ -69,7 +70,6 @@ print(f"The variance of the data is: {round(variance, 2)}")
 rainy_day = max(sum_list)
 rainiest_day = int(sum_list.index(rainy_day))
 
-print(split_data[rainiest_day])
 print(f"\nThe rainiest day was {split_data[rainiest_day][0]} with {round(float(rainy_day) * 0.01, 2)} inches of rain!")
 
 # find the rainiest year
@@ -100,3 +100,41 @@ rainy_year = max(match_dict, key=match_dict.get)
 rainy_year_amount = match_dict[rainy_year]
 
 print(f"\nThe rainiest year was {rainy_year} with {round(float(rainy_year_amount) * 0.01, 2)} inches of rain!")
+
+
+# Version 3 - fun with pyplot!
+
+day_list = []
+
+for i in range(len(split_data)):
+    day_list.append(split_data[i][0])
+
+inches = []
+
+for i in range(len(sum_list)):
+    inches.append(round(sum_list[i] * 0.01, 2))
+
+
+# only printing a month of data as any more is a mess...
+x_values = day_list[0:29]
+y_values = inches[0:29]
+
+plt.plot(x_values, y_values)
+plt.xlabel('day')
+plt.ylabel('inches of rain')
+plt.show()
+
+# print the annual values
+years = []
+annual_inches = []
+
+for key in match_dict:
+    years.append(key)
+    annual_inches.append(match_dict[key] * 0.01)
+
+
+plt.plot(years[1:], annual_inches[1:])      # skip the current year since it's incomplete
+plt.xlabel('year')
+plt.ylabel('inches of rain')
+plt.show()
+
