84972b6943d5155b277869eaa10551be33fdfece
adkinswc <adkinswc@gmail.com>
2018-01-30 16:29:45 -0800

    peaks and valleys

--- Code/Colton/python/lab18_peaks_and_valleys.py
+++ Code/Colton/python/lab18_peaks_and_valleys.py
@@ -1,20 +1,21 @@
 ######finding peaks and valleys#######
 
-data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-
-
 def peaks(data):
-    for i in data[1:20]:
-        if i > data([i]-1) and i > data([i]+1):
-            pass
-    #if the value in the data is greater than the last number and greater than the next, its a peak
-
-
-
+    peak_indices = []
+    for i in range(1,len(data)-1):
+        if data[i] > data[i-1] and data[i] > data[i+1]:
+            peak_indices.append(i)
+    return peak_indices
 def valleys(data):
-    for i in data[1:20]:
-        if i < data([i]-1) and i < data([i]+1):
-            pass
+    valley_indices = []
+    for i in range[1,len(data)-1]:
+        if data[i] < data[i-1] and data[i] < data[i+1]:
+            valley_indices.append(i)
+    return valley_indices
 
 def peaks_and_valleys():
-    pass
+    data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+    print(peaks(data))
+    print(valleys(data))
+
+peaks_and_valleys()
