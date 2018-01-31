d39cf09422fe7af2ff64d5221a88a49eb1631a95
mtgood86 <mtgood86@gmail.com>
2018-01-31 11:08:13 -0800

    added updated labs

--- Code/MichaelG/lab04_magic8ball.py
+++ Code/MichaelG/lab04_magic8ball.py
@@ -3,14 +3,17 @@ answer = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definit
           'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again',
           'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
           'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good,', 'Very doubtful']
-final = random.choice(answer)
+
 print('Welcome')
-user_question = input('Ask a question. ')
-output = final
+question = input('Ask a question. ')
+output = random.choice(answer)
 print(output)
+
 cont = input('Do you wish to ask another question? ')
+
 while cont == 'yes':
-    user_question = input('Ask a question. ')
+    output = random.choice(answer)
+    question = input('Ask a question. ')
     print(output)
     cont = input('Do you wish to ask another question? ')
 else:
