8bf9c867ad77126313e856967a2a6c9dd1a79ec3
Anna <anna@annaspysz.com>
2018-01-31 12:28:29 -0800

    Added version 2

--- Code/Anna/python/optional-road_trip.py
+++ Code/Anna/python/optional-road_trip.py
@@ -3,40 +3,40 @@ Optional lab
 Road Trip
 """
 
-city_to_accessible_cities = {
-    'Boston': {'New York', 'Albany', 'Portland'},
-    'New York': {'Boston', 'Albany', 'Philadelphia'},
-    'Albany': {'Boston', 'New York', 'Portland'},
-    'Portland': {'Boston', 'Albany'},
-    'Philadelphia': {'New York'}
-}
-
-print("This is the list of cities: Boston, New York, Albany, Portland, Philadelphia")
-print("Which city are you starting from?")
-start_city = input("> ")
-all_cities = set()
-
-
-def one_hop(city):
-    hop = city_to_accessible_cities[city]
-    return hop
-
-
-def two_hop(cities):
-    for i in range(0, len(two_hop_cities)):
-        second_city = " and ".join(one_hop(two_hop_cities[i]))
-        print(f"From {cities[i]} you can get to {second_city} in another hop.")
-        all_cities.update(one_hop(two_hop_cities[i]))
-    set_cities = " and ".join(all_cities)
-    print(f"\nSo, all of the cities you could get to in two hops are: {set_cities}!")
-
-
-one_hop_cites = " and ".join(one_hop(start_city))
-two_hop_cities = one_hop_cites.split(' and ')
-
-
-print(f"From {start_city} you can get to {one_hop_cites} in one hop.")
-two_hop(two_hop_cities)
+# city_to_accessible_cities = {
+#     'Boston': {'New York', 'Albany', 'Portland'},
+#     'New York': {'Boston', 'Albany', 'Philadelphia'},
+#     'Albany': {'Boston', 'New York', 'Portland'},
+#     'Portland': {'Boston', 'Albany'},
+#     'Philadelphia': {'New York'}
+# }
+#
+# print("This is the list of cities: Boston, New York, Albany, Portland, Philadelphia")
+# print("Which city are you starting from?")
+# start_city = input("> ")
+# all_cities = set()
+#
+#
+# def one_hop(city):
+#     hop = city_to_accessible_cities[city]
+#     return hop
+#
+#
+# def two_hop(cities):
+#     for i in range(0, len(two_hop_cities)):
+#         second_city = " and ".join(one_hop(two_hop_cities[i]))
+#         print(f"From {cities[i]} you can get to {second_city} in another hop.")
+#         all_cities.update(one_hop(two_hop_cities[i]))
+#     set_cities = " and ".join(all_cities)
+#     print(f"\nSo, all of the cities you could get to in two hops are: {set_cities}!")
+#
+#
+# one_hop_cites = " and ".join(one_hop(start_city))
+# two_hop_cities = one_hop_cites.split(' and ')
+#
+#
+# print(f"From {start_city} you can get to {one_hop_cites} in one hop.")
+# two_hop(two_hop_cities)
 
 
 # Version 2
@@ -57,14 +57,28 @@ start_city = input("> ")
 print("What is your maximum number of hops?")
 max_hops = input("> ")
 max_hops = int(max_hops)
+org_hops = int(max_hops)
 next_cities = []
+travel_time = 0
+hops = 1
 
 
 def city_hopper(start_city, max_hops):
+    global travel_time
+    global hops
     for key in city_to_accessible_cities_with_travel_time[start_city]:
-        if key not in next_cities:
+        if key not in next_cities and hops <= org_hops:
             next_cities.append(key)
+            travel_time += city_to_accessible_cities_with_travel_time[start_city][key]
+            print(f"Your quickest travel time from {start_city} to {key} with {hops} hops would be {travel_time} hours")
+        elif key not in next_cities:
+            next_cities.append(key)
+            travel_time += city_to_accessible_cities_with_travel_time[start_city][key]
+            print(f"Your quickest travel time from {start_city} to {key} with {org_hops} hops would be {travel_time} hours")
+        else:
+            travel_time += 0
     max_hops -= 1
+    hops += 1
     if max_hops != 0:
         for city in next_cities:
             city_hopper(city, max_hops)
@@ -73,4 +87,5 @@ def city_hopper(start_city, max_hops):
 
 hopped_cities = city_hopper(start_city, max_hops)
 
-print(f"From {start_city} you can get to {' and '.join(hopped_cities)} in {max_hops} hops.")
+
+print(f"\nFrom {start_city} you can get to {' and '.join(hopped_cities)} in {max_hops} hops.")
