6a157a581a9b462af06e89af15d151c5ba0eed87
adkinswc <adkinswc@gmail.com>
2018-02-05 15:59:48 -0800

    rain version 2 daily total finished

--- Code/Colton/python/lab24_make_it_rain.py
+++ Code/Colton/python/lab24_make_it_rain.py
@@ -2,6 +2,7 @@ import datetime
 import re #regular expressions module Need to read more into these
 
 
+
 with open('hayden_island.csv') as file:
     lines = file.read().split('\n') # is there a way to import directly from site? Possible idea for capstone
 
@@ -19,7 +20,7 @@ for i in range(len(data)):
     date = datetime.datetime.strptime(data[i][0], '%d-%b-%Y')
     dates.append(f"{date.month}/{date.day}/{date.year}")
 
-print(data)
+#print(data)
 # print(dates)
 
 # Way to use functions in here? Need to use those more
@@ -29,12 +30,35 @@ print(data)
 #Decipher the Hieroglyphics
 # need to find which day had most rain
 # # find which year had the most rainfall on AVERAGE
-rainy_day = []
-for i in range(len(data[i])):
+
+###pull the daily total####
+daily_total = [] #puling all of the rain totals for each day?
+for i in range(len(data)):
+    daily_total.append(data[i][1:2])
+daily_total = [day for dates in daily_total for day in dates]
+
+
+most_rain = max(daily_total) #finds the biggest number
+
+location = daily_total.index(max(daily_total)) # finds the day with the most rain
+
+rainiest_day = (dates[location]) #both date and daily total are the same length so the rain and dates are at the same indices
+
+print(f''' The wetest date was {rainiest_day} with {most_rain} inches of rain.''')
+
+###Pull the rainiest year on average###
 
 
 
-print(rainy_day)
+
+
+
+
+
+
+
+
+
 
 
 
