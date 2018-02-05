86499c77d517c87578d9c8d8f9dbf1d743dd2b0a
michaelx <michaelvertex@gmail.com>
2018-02-05 14:11:17 -0800

    # day one

--- Code/Michael/python/lab23b_contact.py
+++ Code/Michael/python/lab23b_contact.py
@@ -2,16 +2,24 @@ import csv
 
 
 def read():
+    # open the file with the csv module and then read the opened file and split the line breaks and assign it to lines.
     with open('contacts_csv.csv', 'r') as file:
         lines = file.read().split('\n')
+        # we create and empty list where the file contents will be appended to.
         address_book = []
+        # popping the header file we will hold onto its contents as the variable header.
         header = lines.pop(0)
+        # the header is then split at the comma
         header = header.split(',')
+        # the for loop is to iterate over the lines to split contacts at the comma and assign its contents into the variable contact_val
         for contacts in lines:
             contact_val = contacts.split(',')
+            # an empty dictionay is created to hold the contact_val contents we iterate over in the next following for loop.
             my_dict = {}
             for i in range(len(contact_val)):
+                # the contents of each index of control_val is passed into its respective header column and passed to my_dict.
                 my_dict[header[i]] = contact_val[i]
+                # my_dict is then appended to the empty list address_book
             address_book.append(my_dict)
         print(address_book)
     return address_book
@@ -28,13 +36,13 @@ def create():
             input_contact.append(input(f'enter new contact favorite fruit: \n> '))
             input_contact.append(input(f'Enter new contact favorite color: \n> '))
             address_book.writerow(input_contact)
-        answer = input('would you like to create another contact?')
+        answer = input('would you like to create another contact? Yes or no \n> ')
         return address_book
 
 
 def retrieve(address_book):
 
-    retrieve = input('Enter a name to retrieve their information:')
+    retrieve = input('Enter a name to retrieve their information:\n> ')
 
     for i in range(len(address_book)):
         if retrieve in address_book[i]['name']:
@@ -44,9 +52,9 @@ def retrieve(address_book):
 
 def update(address_book):
 
-    update_name = input('Whose information would you like to update? ')
-    update_key = input('Which key would you like to change? ')
-    update_value = input('What is the new value? ')
+    update_name = input('Whose information would you like to update?\n> ')
+    update_key = input('Which key would you like to change?\n> ')
+    update_value = input('What is the new value?\n> ')
 
     for i in range(len(address_book)):
         if update_name in address_book[i]['name']:
@@ -56,17 +64,20 @@ def update(address_book):
 
 
 def delete(address_book):
-    del_name = input('Whose information would you like to delete?')
+    del_name = input('Whose information would you like to delete?\n> ')
 
     for i in range(len(address_book) - 1):
         if del_name in address_book[i]['name']:
             del address_book[i]
-            print(f'You have deleted {del_name} from the contacts list forever.')
+            print(f'You have deleted {del_name} from your contacts list.')
 
     print(address_book)
 
 
 def main():
+    '''
+    main is the control mechanism where functions are called on to execute its sub-operations. The while loop keeps executing until option 6 is selcted and then exit is called to quit.
+    '''
     print('''
     What would you like to do? Select one.
     1. Read the current contacts
--- Code/Michael/python/lab24_raindata.py
+++ Code/Michael/python/lab24_raindata.py
@@ -1,3 +1,26 @@
+import re
+from datetime import datetime
+
+# open .txt file and splits the file at the line breaks and assigns it to the variable contents, this portion works
+with open('raindata_scratch.txt', 'r') as file:
+    contents = file.read().split('\n')
+    # print(contents)
 
 
+data_l = []
+
+for i in range(len(contents)):
+    data_l.append(re.split('\s+', contents[i]))
+    # print(contents)
+
+date_l = []
+
+for dates in range(len(data_l)):
+    if len(data_l[dates][0]) > 11:
+        date_l.append(datetime.strptime(data_l[dates][0], '%d/%b/%y'))
+
+print(data_l)
+
+for i in range(len(date_l)):
+    print(date_l[i].year, date_l[i].month, date_l[i].day)
 
