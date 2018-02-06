c241c6dd58a5b3e69e264f9d000b2236249d4102
Brianna Harris <Brianna@BriannaHarris.net>
2018-02-06 10:13:06 -0800

    Putting character and object classes in a seperate file for now. In the process of reorganizing code for usability/readability.

--- Code/Brianna/Python/Lab_26_adventure_game/Lab_26_adventure_game.py
+++ Code/Brianna/Python/Lab_26_adventure_game/Lab_26_adventure_game.py
@@ -11,111 +11,50 @@ import pygame
 
 def draw_stick_figure(screen, x, y):
     # Head
-    pygame.draw.ellipse(screen, black, [1 + x, y, 10, 10], 0)
+    pygame.draw.ellipse(screen, BLACK, [1 + x, y, 10, 10], 0)
 
     # Legs
-    pygame.draw.line(screen, black, [5 + x, 17 + y], [10 + x, 27 + y], 2)
-    pygame.draw.line(screen, black, [5 + x, 17 + y], [x, 27 + y], 2)
+    pygame.draw.line(screen, BLACK, [5 + x, 17 + y], [10 + x, 27 + y], 2)
+    pygame.draw.line(screen, BLACK, [5 + x, 17 + y], [x, 27 + y], 2)
 
     # Body
-    pygame.draw.line(screen, red, [5 + x, 17 + y], [5 + x, 7 + y], 2)
+    pygame.draw.line(screen, RED, [5 + x, 17 + y], [5 + x, 7 + y], 2)
 
     # Arms
-    pygame.draw.line(screen, red, [5 + x, 7 + y], [9 + x, 17 + y], 2)
-    pygame.draw.line(screen, red, [5 + x, 7 + y], [1 + x, 17 + y], 2)
+    pygame.draw.line(screen, RED, [5 + x, 7 + y], [9 + x, 17 + y], 2)
+    pygame.draw.line(screen, RED, [5 + x, 7 + y], [1 + x, 17 + y], 2)
 
-class Character():
-    """ This is a class that represents the main character in a game. """
-
-    def __init__(self):
-        """ Setting up a starting point for our character """
-        self.name = "Briar"
-        self.max_hit_points = 50
-        self.current_hit_points = 50
-        self.max_speed = 10
-        self.armor_amount = 8
-        self.calories = 500
-
-    def eat(self, calories):
-        self.calories += calories
-        return self.calories
-
-    def sleep(self, sleep=100):
-        if sleep == 100:
-            self.max_hit_points = 50
-        return
-
-    def attack(self):
-        pass
-
-class Willowisp():
-    """ This sets up the methods and 'Character' that tasks are given to the player through. """
-    def __init__(self):
-        self.name = "Willow"
-        self.location = # Random location within the area just outside of main characters home.
-        self.converse = converse  # figure out how to do this.
+"""
 
 
-class Enemies():
-    """ This class sets up the enemies for the main character to encounter """
-
-    def __init__(self):
-        self.max_hit_points = 20
-        self.current_hit_points = 20
-        self.max_speed = 10
-        self.armour_amount = 8
-
-    def attack(self):
-        # an if statement
-        pass
-
-
-class Cave_monster(Enemies):  # Common and can move around. You can see them before you run into them.
-    pass
-
-
-class Bandit(Enemies):  # Uncommon and you cannot see them unless you run into them. Possibly move around.
-    pass
-
-
-class Found_items():   # Found items can be put into a sack/inventory and used later.
-    """ This class sets up the kinds of items our main character can pick up"""
-    def __init__(self, name):
-        self.name = name
-        self.place_in_sack = True
-        self.used = False
-
-
-
-class Food():
-    def __init__(self, name, calories):
-        self.name = name
-        self.calories = calories
-        self.place_in_sack = True
-        self.used = False
-
-
-ham = Food('ham', 230)
-casserole = Food('casserole', 100)
-can_of_soup = Food('can of soup', 50)
-
-
+"""
 
 
 pygame.init()
+
+
+# ----- Global constants -----
+
 #         R    G    B
-white = (255, 255, 255)
-mid_green = (0, 135, 27)
-forest_green = (1, 96, 20)
-leaf_green = (15, 198, 5)
-yellow = (241, 255, 0)
-blue = (80, 255, 239)
-purple = (203, 0, 255)
-red = (237, 28, 36)
+WHITE = (255, 255, 255)
+MID_GREEN = (0, 135, 27)
+FOREST_GREEN = (1, 96, 20)
+LEAF_GREEN = (15, 198, 5)
+YELLOW = (241, 255, 0)
+BLUE = (80, 255, 239)
+PURPLE = (203, 0, 255)
+RED = (237, 28, 36)
+BLACK = (0, 0, 0)
 
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 500
 
+# ------------------------------
+
+# Set font
+font = pygame.font.Font(None, 36)
 # Set width and height of the screen. First number is width, second is height.
-size = [700, 500]
+size = [SCREEN_WIDTH, SCREEN_HEIGHT]
 screen = pygame.display.set_mode(size) # can put size directly in (size) area. Screen is the variable name we have assigned this window to. We can create more windows.
 
 pygame.display.set_caption("Programmer's Escape to Hidden Mountain")
@@ -126,16 +65,81 @@ done = False  # Loop control
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()  # Controls how fast the game runs
 
-'''# Make the cursor transparent.
-mouse_cursor = pygame.mouse.set_cursor(pygame.mouse.set_cursor((8, 8), (0, 0), (0, 0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0, 0)))
-
+'''
 # Make an image follow the cursor around. Need to have an image file here.
 cursor_picture = pygame.image.load('./cursor.png').convert_alpha()'''
 
 # Hide the mouse cursor
 pygame.mouse.set_visible(0)
 
+# Speed in pixels per frame
+x_speed = 0
+y_speed = 0
 
+# Current position
+x_coord = 10
+y_coord = 10
+
+""""# Need to download from http://freemusicarchive.org/music/MIT_Concert_Choir and put in folder to point to.
+pygame.mixer.music.load('MIT_Concert_Choir_O_Fortuna.ogg')
+pygame.mixer.music.set_endevent(pygame.constants.USEREVENT)
+pygame.mixer.music.play()
+
+# This is a font we use to draw text on the screen (size 36)
+#font = pygame.font.Font(None, 36)"""
+
+
+""""# Use this boolean variable to trigger if the game is over.
+game_over = False
+    if game_over:
+        # If game over is true, draw game over
+        text = font.render("Game Over", True, WHITE)
+        text_rect = text.get_rect()
+        text_x = screen.get_width() / 2 - text_rect.width / 2
+        text_y = screen.get_height() / 2 - text_rect.height / 2
+        screen.blit(text, [text_x, text_y])
+ """
+
+display_instructions = True
+instruction_page = 1
+
+# -------- Instruction Page Loop -----------
+while not done and display_instructions:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            instruction_page += 1
+            if instruction_page == 3:
+                display_instructions = False
+
+    # Set the screen background
+    screen.fill(BLACK)
+
+    if instruction_page == 1:
+        # Draw instructions, page 1
+        # This could also load an image created in another program.
+        # That could be both easier and more flexible.
+
+        text = font.render("Welcome to Escape to Code Mountain", True, WHITE)
+        screen.blit(text, [10, 10])
+
+        text = font.render("Page 1", True, WHITE)
+        screen.blit(text, [10, 40])
+
+    if instruction_page == 2:
+        # Draw instructions, page 2
+        text = font.render("This program has instructions. Hopefully they aren't complicated. ", True, WHITE)
+        screen.blit(text, [10, 10])
+
+        text = font.render("Page 2", True, WHITE)
+        screen.blit(text, [10, 40])
+
+    # Limit to 60 frames per second
+    clock.tick(60)
+
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
 
 # -------- Main Program Loop -----------
 while not done:
@@ -143,22 +147,43 @@ while not done:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            pygame.mouse.get_pos()
-            pass
-        elif event.type == pygame.MOUSEBUTTONUP:
-            pygame.mouse.get_pos()
-            pass
-        elif event.type == pygame.MOUSEMOTION:
-            pygame.mouse.set_visible(True)
-            pygame.mouse.get_pos()
-            pass
+
+        elif event.type == pygame.KEYDOWN:
+            # Figure out if it was an arrow key. If so
+            # adjust speed.
+            if event.key == pygame.K_LEFT:
+                x_speed = -3
+            elif event.key == pygame.K_RIGHT:
+                x_speed = 3
+            elif event.key == pygame.K_UP:
+                y_speed = -3
+            elif event.key == pygame.K_DOWN:
+                y_speed = 3
+
+        # User let up on a key
+        elif event.type == pygame.KEYUP:
+            # If it is an arrow key, reset vector back to zero
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_speed = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                y_speed = 0
+
+        elif event.type == pygame.constants.USEREVENT:
+            # This event is triggered when the song stops playing.
+            #
+            # Next, play "Alone" by Saito Koji
+            # Available from:
+            # http://freemusicarchive.org/music/Saito_Koji/Again/01-Alone
+            pygame.mixer.music.load('Saito_Koji_-_01_-_Alone.ogg')
+            pygame.mixer.music.play()
 
 
 
     # --- Game logic should go here
 
-
+    # Move the object according to the speed vector.
+    x_coord = x_coord + x_speed
+    y_coord = y_coord + y_speed
 
 
     # --- Drawing code should go here
@@ -169,17 +194,19 @@ while not done:
     # Render the text. "True" means anti-aliased text.
     # Black is the color. This creates an image of the
     # letters, but does not put it on the screen
-    text = font.render("Score: " + str(score), True, black)
+    #text = font.render("Score: " + str(score), True, black)
 
     # First, clear the screen to white. Don't put other drawing commands
     # above this, or they will be erased with this command.
-    screen.fill(leaf_green)
+    screen.fill(LEAF_GREEN)
     for x in range(0, 700, 20):
-        pygame.draw.line(screen, mid_green, [x, 0], [x, 500], 10)
+        pygame.draw.line(screen, MID_GREEN, [x, 0], [x, 500], 10)
 
     # Putting the mouse on the screen?
-    screen.blit(mouse_cursor, pygame.mouse.get_pos())
-    pygame.display.update()
+    #screen.blit(mouse_cursor, pygame.mouse.get_pos())
+    #pygame.display.update()
+
+    draw_stick_figure(screen, x_coord, y_coord)
     # ---  update the screen with what we've drawn.
     pygame.display.flip() # Like a flip book. Flips things to the screen. Do after drawing.
 
