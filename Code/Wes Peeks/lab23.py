07d249e95ce3be18d19f161b964d006b9ebade46
mtnsct189 <wwpeeks@gmail.com>
2018-02-06 16:39:53 -0800

    create file

--- Code/Wes Peeks/lab23.py
+++ Code/Wes Peeks/lab23.py
@@ -1,2 +1,72 @@
-with open('contacts.csv', 'r') as file:
-    file = file.read().split('\\')
+#Let's build a program to manage a list of contacts. To start, we'll build a CSV ('comma separated values') together,
+#and go over how to load that file.
+#Headers might consist of name, favorite fruit, favorite color. Open the CSV,
+#convert the lines of text into a list of dictionaries, one dictionary for each user.
+#The text in the header represents the keys, the text
+#
+
+
+
+def load_contacts():
+    with open('contacts_csv.csv', 'r') as file:
+        lines = file.read().split('\n')
+        print(lines)
+    contacts = []
+    headers = lines.pop(0)
+    headers = headers.split(',')
+    for line in lines:
+        line = line.split(',')
+        new_dict = {}
+        for i in range(len(line)):
+            new_dict[headers[i]] = line[i]
+        contacts.append(new_dict)
+    print(contacts)
+    return contacts
+
+'''Implement a CRUD REPL
+
+Create a record: ask the user for each attribute, add a new contact to your contact list with the attributes that the user entered.
+Retrieve a record: ask the user for the contact's name, find the user with the given name, and display their information
+Update a record: ask the user for the contact's name, then for which attribute of the user they'd like to update and the value of the attribute they'd like to set.
+Delete a record: ask the user for the contact's name, remove the contact with the given name from the contact list.
+'''
+
+
+def create(address_book):
+
+    add_name = input('What contact name?\n:')
+    add_fruit = input('What fruit?\n:')
+    add_color = input('What color?')
+
+    address_book.append({'name': add_name, 'favorite fruit': add_fruit, 'favorite color': add_color})
+
+
+
+def retrieve(address_book):
+
+    retrieve_name = input('name lookup')
+    for name in address_book:
+        if retrieve_name in name.values():
+            print(name)
+
+
+def update(address_book):
+
+    retrieve_name = input('name of contact to change: ')
+    update_name = input('contact\'s new name: ')
+    for contact in address_book:
+        if contact['name'] == retrieve_name:
+            contact['name'] = update_name
+            break
+
+
+def delete(address_book):
+
+    delete_name = input('Which name would you like to delete?')
+
+
+
+
+address_book = load_contacts()
+#retrieve()
+update(address_book)
--- Code/Wes Peeks/prac2.py
+++ Code/Wes Peeks/prac2.py
@@ -1,25 +1,67 @@
 # Get a string from the user, print out another string, doubling every letter.
 
-user = str(input('Your words\n:'))
+# user = str(input('Your words\n:'))
+import string
 
 def double_string(user):
-        user1 = ''# empty string for return
-        for char in list(user):# loop through characters by converting to list
-            user1 += char * 2# user1 empty string gets characters times 2
+        user1 = ''  # empty string for return
+        for char in list(user):  # loop through characters by converting to list
+            user1 += char * 2  # user1 empty string gets characters times 2
         print(user1)
 
 
-double_string(user)
+# double_string(user)
 
 #Write a function that takes a string, and returns a list of strings, each missing a different character.
-s = input('Word')
+#s = input('Word')
+
 
 def weird_string(s):
-    s1 = []
-    for char in list(s):
+    s = input('Word')
+    output = []
+    for char in range(len(s)):
         s = list(s)
-        s1 = char + s.pop(randint)
+        s1 = s.copy()
+        s1.pop(char)
+        s1 = ''.join(s1)
+        output.append(s1)
+    print(output)
 
-        print(s1)
+#weird_string(s)
 
-weird_string(s)
+
+#Return the letter that appears the latest in the english alphabet.
+
+
+
+def alphebet_letter():
+    a = string.ascii_lowercase
+    word = input('Word')
+    for char in a[::-1]:
+        if char in word:
+            return char
+
+#print(alphebet_letter())
+#Write a function that returns the number of occurances of 'hi' in a given string.
+
+
+
+def count_hi(x, y):
+    # repeat = string.input(hi)
+    print(x.count(y))
+
+
+repeat = input('gimmie a string: ')
+count_hi(repeat, "hi")
+
+#Write a function that returns True if a given string contains the same number of 'cat' as it does 'dog'
+
+
+
+def cat_dog(uinput):
+    if uinput.count('cat') == uinput.count('dog'):
+        return True
+    else:
+        return False
+
+print(cat_dog('catdogcat'))
