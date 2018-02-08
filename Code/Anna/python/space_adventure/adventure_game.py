772718ec36bccbcd32a6cf68b6e983b69f5fca07
Anna <anna@annaspysz.com>
2018-02-07 16:38:13 -0800

    Trying to get everything to work in modules...

--- Code/Anna/python/space_adventure/adventure_game.py
+++ Code/Anna/python/space_adventure/adventure_game.py
@@ -17,10 +17,8 @@ from __future__ import print_function
 import chalk
 import random
 from time import sleep
-import pyaudio
-import wave
-import threading
 from intro_outro import intro, outro, outro_alt
+from audio_classes import *
 
 
 class Game:
@@ -87,126 +85,6 @@ class Game:
         player_x = 10
         player_y = 4
         return player_x, player_y
-
-
-class WavePlayer(threading.Thread):
-    """
-    A simple class based on PyAudio to play a sine wave.
-    It's a threading class. You can play audio while your application
-    continues to do stuff.
-    """
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.p = pyaudio.PyAudio()
-
-        self.fs = 44100          # sampling rate, Hz, must be integer
-
-    def run(self):
-        """
-        Just another name for self.start()
-        """
-        # define stream chunk
-        chunk = 1024
-
-        # open a wav format music
-        f = wave.open(r"audio/beginning_theme.wav", "rb")
-        # instantiate PyAudio
-        p = pyaudio.PyAudio()
-        # open stream
-        stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
-                        channels=f.getnchannels(),
-                        rate=f.getframerate(),
-                        output=True)
-        # read data
-        data = f.readframes(chunk)
-
-        # play stream
-        while data:
-            stream.write(data)
-            data = f.readframes(chunk)
-
-        # stop audio stream
-        stream.stop_stream()
-        stream.close()
-
-        # close PyAudio
-        p.terminate()
-
-    def sound_effect(self, path):
-        """
-        Use for sound effects throughout the game
-        """
-        # define stream chunk
-        chunk = 1024
-
-        # open a wav format music
-        f = wave.open(path, "rb")
-        # instantiate PyAudio
-        p = pyaudio.PyAudio()
-        # open stream
-        stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
-                        channels=f.getnchannels(),
-                        rate=f.getframerate(),
-                        output=True)
-        # read data
-        data = f.readframes(chunk)
-
-        # play stream
-        while data:
-            stream.write(data)
-            data = f.readframes(chunk)
-
-        # stop audio stream
-        stream.stop_stream()
-        stream.close()
-
-        # close PyAudio
-        p.terminate()
-
-
-class BonusWave(threading.Thread):
-    """
-    Another wave player for the bonus ending, so the
-    ending will run while the audio plays
-    """
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.p = pyaudio.PyAudio()
-
-        self.fs = 44100          # sampling rate, Hz, must be integer
-
-    def run(self):
-        """
-        Just another name for self.start()
-        """
-        # define stream chunk
-        chunk = 1024
-
-        # open a wav format music
-        f = wave.open(r"audio/cantina.wav", "rb")
-        # instantiate PyAudio
-        p = pyaudio.PyAudio()
-        # open stream
-        stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
-                        channels=f.getnchannels(),
-                        rate=f.getframerate(),
-                        output=True)
-        # read data
-        data = f.readframes(chunk)
-
-        # play stream
-        while data:
-            stream.write(data)
-            data = f.readframes(chunk)
-
-        # stop audio stream
-        stream.stop_stream()
-        stream.close()
-
-        # close PyAudio
-        p.terminate()
 
 
 def difficulty_setting():
@@ -425,9 +303,9 @@ def game_on(player_x, player_y, num_enemies):
 
 
 # this is where we finally launch the game
-
 s = WavePlayer()                                            # initialize intro music
 b = BonusWave()                                             # initialize music for bonus game
+
 intro()                                                     # start the intro to the game
 diff_setting = difficulty_setting()         # prompt the user for the difficulty setting, which determines # of enemies
 asteroids = int(diff_setting * 2)                           # num of asteroids based on diff setting
