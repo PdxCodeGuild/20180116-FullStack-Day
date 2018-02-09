8eae88b00bdab532a3012b45f9fd835708a9c1d9
Anna <anna@annaspysz.com>
2018-02-08 21:59:47 -0800

    The finished version of the blog, without templates (for now)

--- Code/Anna/HTML CSS/lab02-blog/script.js
+++ Code/Anna/HTML CSS/lab02-blog/script.js
@@ -1,12 +1,12 @@
 // A $( document ).ready() block.
+"use strict";
+
 $( document ).ready(function() {
     console.log( "ready!" );
     $(".button-collapse").sideNav();
     $('.parallax').parallax();
-});
-
-$(function() {
-  $('a.smooth-scroll[href*="#"]:not([href="#"])').click(function() {
+    $('.modal').modal();
+    $('a.smooth-scroll[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
