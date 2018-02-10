2198ebfa034638bcd2be8b9182a30ebc1d0f1b59
adkinswc <adkinswc@gmail.com>
2018-02-09 17:15:59 -0800

    ATM, cannot figure out why it wants to do withdraw

--- Code/Colton/python/lab_25_ATM.py
+++ Code/Colton/python/lab_25_ATM.py
@@ -1,24 +1,75 @@
 class ATM:
-    def __init__(self, balance = 0.00, interest_rate = .1):
-        self.balance
-        self.interest_rate
+    def __init__(self, balance):
+        self.balance = balance
+        self.transactions = []
 
+    def main_menu(self):
+        while True:
+            user = input("Welcome to the bank of capitalist pigs. Pick an option\n Balance\nCheck withdraw\nDeposit\nWithdraw\nPrint Transactions\n:")
+            if user == 'balance':
+                ATM.check_balance(self)
+            elif user == 'check withdraw' or 'check':
+                ATM.check_withdrawal(self)
+            elif user == 'deposit':
+                ATM.deposit(self)
+            elif user == 'withdraw':
+                ATM.withdraw(self)
+            elif user == 'print transaction' or 'print':
+                ATM.print_transactions(self)
+            elif user == 'done' or 'quit':
+                quit()
+            else:
+                print("Invalid input. Try again")
+                atm.main_menu()
 
     def check_balance(self):
-        return f'''Your account balance is {self.balance}'''
+        return print(f'''Your account balance is {self.balance}''')
 
-    def deposit(amount):
-        self.balance += amount
-    def check_withdrawal(amount):
-        if amount <= self.balance:
-            return f'''You may make a withdrawal'''
+    def deposit(self):
+        deposit_amount = float(input("How much do you want to deposit?"))
+        self.balance += deposit_amount
+        self.transactions.append(f'''User deposited ${deposit_amount}.''')
+        return print( f'''You deposited ${deposit_amount}. Your new balance is {self.balance}''')
+
+
+    def check_withdrawal(self):
+        check_amount = float(input("How much do you want to withdraw?"))
+        if check_amount <= self.balance:
+            return print(f'''You may make a withdrawal''')
         else:
-            return f'''You broke mutha fucka'''
-        
+            return print(f'''You broke mutha fucka''')
 
-    def withdraw(amount):
-        self.balance -= amount
+    def withdraw(self):
+        withdraw_amount = float(input("How much do you want to withdraw?"))
+        if withdraw_amount <= balance:
+            self.balance -= amount
+            self.balance.append(f'''User withdrew ${withdraw_amount}.''')
+            return print(f'''You withdrew ${withdraw_amount}. Your new balance is ${balance}.''')
+        else:
+            return print(f'''You dont have enough money dummy.''')
 
     def calc_interest(self):
         self.balance += self.balance * self.interest_rate
+
+
+
+    def print_transactions(self):
+        return print(f'''Users transactions \n...\n'''.join(self.transactions))
+
+
+
+
+
+
+atm = ATM(1000000.00)
+atm.main_menu()
+
+
+
+
+
+
+
+
+
 
