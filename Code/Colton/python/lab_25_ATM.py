30d0bc5e42fc6b0651983d5e23b2e3c38927f9b2
adkinswc <adkinswc@gmail.com>
2018-02-09 18:04:21 -0800

    ATM works

--- Code/Colton/python/lab_25_ATM.py
+++ Code/Colton/python/lab_25_ATM.py
@@ -3,25 +3,6 @@ class ATM:
         self.balance = balance
         self.transactions = []
 
-    def main_menu(self):
-        while True:
-            user = input("Welcome to the bank of capitalist pigs. Pick an option\n Balance\nCheck withdraw\nDeposit\nWithdraw\nPrint Transactions\n:")
-            if user == 'balance':
-                ATM.check_balance(self)
-            elif user == 'check withdraw' or 'check':
-                ATM.check_withdrawal(self)
-            elif user == 'deposit':
-                ATM.deposit(self)
-            elif user == 'withdraw':
-                ATM.withdraw(self)
-            elif user == 'print transaction' or 'print':
-                ATM.print_transactions(self)
-            elif user == 'done' or 'quit':
-                quit()
-            else:
-                print("Invalid input. Try again")
-                atm.main_menu()
-
     def check_balance(self):
         return print(f'''Your account balance is {self.balance}''')
 
@@ -29,7 +10,7 @@ class ATM:
         deposit_amount = float(input("How much do you want to deposit?"))
         self.balance += deposit_amount
         self.transactions.append(f'''User deposited ${deposit_amount}.''')
-        return print( f'''You deposited ${deposit_amount}. Your new balance is {self.balance}''')
+        return print(f'''You deposited ${deposit_amount}. Your new balance is {self.balance}''')
 
 
     def check_withdrawal(self):
@@ -41,10 +22,10 @@ class ATM:
 
     def withdraw(self):
         withdraw_amount = float(input("How much do you want to withdraw?"))
-        if withdraw_amount <= balance:
-            self.balance -= amount
-            self.balance.append(f'''User withdrew ${withdraw_amount}.''')
-            return print(f'''You withdrew ${withdraw_amount}. Your new balance is ${balance}.''')
+        if withdraw_amount <= self.balance:
+            self.balance -= withdraw_amount
+            self.transactions.append(f'''User withdrew ${withdraw_amount}.''')
+            return print(f'''You withdrew ${withdraw_amount}. Your new balance is ${self.balance}.''')
         else:
             return print(f'''You dont have enough money dummy.''')
 
@@ -56,7 +37,26 @@ class ATM:
     def print_transactions(self):
         return print(f'''Users transactions \n...\n'''.join(self.transactions))
 
+    def main_menu(self):
+        while True:
+            user = input("Welcome to the bank of capitalist pigs. Pick an option\n Balance\nCheck withdraw\nDeposit\nWithdraw\nPrint Transactions\n:")
+            if user == 'balance':
+                ATM.check_balance(self)
+            elif user == 'check':#for some reason I cant make this an or statment to include 'check', was originally checck transaction or check
+                ATM.check_withdrawal(self)
+            elif user == 'deposit':
+                ATM.deposit(self)
+            elif user == 'withdraw':
+                ATM.withdraw(self)
+            elif user == 'print':
+                ATM.print_transactions(self)
+            elif user == 'done':
+                quit()
+            else:
+                print("Invalid input. Try again")
+                atm.main_menu()
 
+###Cant figure our why I cant use or statements in the function
 
 
 
