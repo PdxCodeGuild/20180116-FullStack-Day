e9fb0a035abed839582efb7e926e7f7e73539cab
Anna <anna@annaspysz.com>
2018-02-15 16:01:20 -0800

    Screw you, AJAX

--- Code/Anna/HTML CSS/lab05-burrito_order_form/js/main.js
+++ Code/Anna/HTML CSS/lab05-burrito_order_form/js/main.js
@@ -21,59 +21,59 @@ $( document ).ready(function() {
   });
 });
 
-// Function for sending all of the forms
-window.addEventListener("load", function () {
-  function sendData() {
-    var XHR = new XMLHttpRequest();
-
-    // Bind the FormData object and the form element
-    var FD = new FormData(form);
-
-    // Define what happens on successful data submission
-    XHR.addEventListener("load", function(event) {
-      alert(event.target.responseText);
-    });
-
-    // Define what happens in case of error
-    XHR.addEventListener("error", function(event) {
-      alert('Oups! Something goes wrong.');
-    });
-
-    // Set up our request
-    XHR.open("POST", "https://requestb.in/1f84cwu1");
-
-    // The data sent is what the user provided in the form
-    XHR.send(FD);
-  }
-
-  // Access the form element...
-  var form1 = document.getElementById("form1");
-  var form2 = document.getElementById("form2");
-  var form3 = document.getElementById("form3");
-  var form4 = document.getElementById("form4");
-
-  // ...and take over its submit event.
-  form1.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    sendData();
-  });
-  form2.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    sendData();
-  });
-  form3.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    sendData();
-  });
-  form4.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    sendData();
-  });
-});
+// // Function for sending all of the forms
+// window.addEventListener("load", function () {
+//   function sendData() {
+//     var XHR = new XMLHttpRequest();
+//
+//     // Bind the FormData object and the form element
+//     var FD = new FormData(form);
+//
+//     // Define what happens on successful data submission
+//     XHR.addEventListener("load", function(event) {
+//       alert(event.target.responseText);
+//     });
+//
+//     // Define what happens in case of error
+//     XHR.addEventListener("error", function(event) {
+//       alert('Oups! Something goes wrong.');
+//     });
+//
+//     // Set up our request
+//     XHR.open("POST", "https://requestb.in/1f84cwu1");
+//
+//     // The data sent is what the user provided in the form
+//     XHR.send(FD);
+//   }
+//
+//   // Access the form element...
+//   var form1 = document.getElementById("form1");
+//   var form2 = document.getElementById("form2");
+//   var form3 = document.getElementById("form3");
+//   var form4 = document.getElementById("form4");
+//
+//   // ...and take over its submit event.
+//   form1.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//
+//     sendData();
+//   });
+//   form2.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//
+//     sendData();
+//   });
+//   form3.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//
+//     sendData();
+//   });
+//   form4.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//
+//     sendData();
+//   });
+// });
 
 
 //
