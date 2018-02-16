6b712ff67754a022ac7ed07b90003b9720c288ca
ninjabee <bkharris77@gmail.com>
2018-02-16 13:24:03 -0800

    palindome and anagram update

--- Code/Brianna/JavaScript/anagram_palendroms/javaScript.js
+++ Code/Brianna/JavaScript/anagram_palendroms/javaScript.js
@@ -1,27 +1,30 @@
 
-var word = prompt("Type in a word to check and see if it's a palendrome.");
+var word = prompt("Type in a word to check and see if it's a palindrome.");
 
-function check_palendrome(a_string); {
-    if word == word.reverse();
-    alert = ("This is a palendrome.")
-  } else {
-        alert = ("This is not a palendrome.")
-}
+function check_palendrome(a_string)
+{
+    if (a_string === a_string.reverse()) {
+        alert("This is a palendrome.");
+    }
+    else {
+        alert("This is not a palendrome.");
+    }
+};
 
-
+check_palendrome(word);
 
 
 var regularize = function(word) {
     return word.toLowerCase().replace(/[^a-z\d]+/g, '').split('').sort().join('').trim();
-}
+};
 
 
-this.isAnagram = regularize(this.anagram_one) === regularize(this.anagram_two);
-
-var anagram_one = prompt("Type in first entry for anagram check.")
-var anagram_two = prompt("Type in second entry for anagram check.")
 
 
+var anagram_one = prompt("Type in first entry for anagram check.");
+var anagram_two = prompt("Type in second entry for anagram check.");
+
+var isAnagram = regularize(anagram_one) === regularize(anagram_two);
 //function check_anagram(first_string, second_string); {
 //    var first_string =  regularize(this.anagram_one)
 //    var second_string = regularize(this.anagram_two)
@@ -31,8 +34,10 @@ var anagram_two = prompt("Type in second entry for anagram check.")
 
 
 
-check_anagram(anagram_one, anagram_two);
-  if isAnagram {
-      alert = ("This is an anagram!")
-}
+
+
+if (isAnagram) {
+    alert("This is an anagram!");
+};
+
 
