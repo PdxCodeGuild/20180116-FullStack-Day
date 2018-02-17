afe3761b268b08be2a21ffa765a1a2abc5660222
Anna <anna@annaspysz.com>
2018-02-17 15:30:27 -0800

    Fixed the generate button problem, added floating emojis :D

--- Code/Anna/JavaScript/lab01-random_emoticon_generator/js/main.js
+++ Code/Anna/JavaScript/lab01-random_emoticon_generator/js/main.js
@@ -33,27 +33,29 @@ let sad = ['(', '[', '{', '(', '<']
 let meh = ['O', '0', 'o', '%', '#', '@', '*', '|']
 let rotated = false;
 
-function generator() {
-    let randomEyes = eyes[Math.floor(Math.random() * eyes.length)];
-    let randomNose = noses[Math.floor(Math.random() * noses.length)];
-    let randomMouth = mouths[Math.floor(Math.random() * mouths.length)];
+// function generator() {
+//     let randomEyes = eyes[Math.floor(Math.random() * eyes.length)];
+//     let randomNose = noses[Math.floor(Math.random() * noses.length)];
+//     let randomMouth = mouths[Math.floor(Math.random() * mouths.length)];
+//
+//     let eyeOutput = document.querySelector('#eye');
+//     let noseOutput = document.querySelector('#nose');
+//     let mouthOutput = document.querySelector('#mouth');
+//
+//     eyeOutput.innerText = randomEyes;
+//     noseOutput.innerText = randomNose;
+//     mouthOutput.innerText = randomMouth;
+//
+//     // document.getElementById('generator_div').className = 'animate';
+//
+//     $('#generator_div').toggleClass('rotated');
+//     console.log("I should be done now.");
+// }
 
-    let eyeOutput = document.querySelector('#eye');
-    let noseOutput = document.querySelector('#nose');
-    let mouthOutput = document.querySelector('#mouth');
-
-    eyeOutput.innerText = randomEyes;
-    noseOutput.innerText = randomNose;
-    mouthOutput.innerText = randomMouth;
-
-    // document.getElementById('generator_div').className = 'animate';
-
-    $('#generator_div').toggleClass('rotated');
-    console.log("I should be done now.");
-}
-
+// start the animations
 
 $(document).ready(function() {
+    console.log("ready!");
     $('#clicker').click(function() {
         let randomEyes = eyes[Math.floor(Math.random() * eyes.length)];
         let randomNose = noses[Math.floor(Math.random() * noses.length)];
@@ -71,6 +73,14 @@ $(document).ready(function() {
 
         $('#rotating').toggleClass('rotated');
         console.log("I should be done now.");
+    });
+    $('#more').on('click', function(){
+        for (let i=1; i<11; ++i) {
+            let element = document.querySelector('.x'+i);
+            element.style.AnimationPlayState = "running";
+            element.style.webkitAnimationPlayState = "running";
+        }
+        console.log("started the floating emojis");
     });
 });
 
