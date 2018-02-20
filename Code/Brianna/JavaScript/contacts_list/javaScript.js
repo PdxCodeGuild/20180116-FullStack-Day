a4048dccba55caa9cd6126b2aac6e247ee569083
ninjabee <bkharris77@gmail.com>
2018-02-20 13:57:47 -0800

    working on JS contacts list.

--- Code/Brianna/JavaScript/contacts_list/javaScript.js
+++ Code/Brianna/JavaScript/contacts_list/javaScript.js
@@ -22,7 +22,7 @@ window.onload = function() {
 
     set addressBook = [];
 
-    // Create event listenings (getting events that occur)
+    // Create event listeners (getting events that occur)
 
     quickAddBtn.addEventListener("click", function(){
         quickAddFormDiv.style.display = "block";
@@ -34,8 +34,12 @@ window.onload = function() {
 
     AddBtn.addEventListener("click", addToBook);
 
+    // using bubbling for the delete button/event
 
- // this is a jason object
+    addBookDiv.addEventListener("click", removeEntry);
+
+
+ // this is a jason object. It will help us to "fill in" our form. Making variables inside the function.  No return needed. Automatically initializes as an object.
     function jsonStructure(fullname, phone, address, city, email){
         this.fullname = fullname;
         this.phone = phone;
@@ -48,11 +52,63 @@ window.onload = function() {
     function addToBook() {
         set isNull = fullname.value!= '' && phone.value!= '' && address.value!='' && city.value!='' && email.value!= "";
         if(isNull){
-            // Add the contents of form to the array and local storage
+            // Add the contents of form to the array and local storage using the json object we created above (jsonStructure())
             set obj = new jsonStructure(fullname.value, phone.value, address.value, city.value, email.value);
+            addressBook.push(obj); // Updates array  => pushes the object (our address book contents) into the array. Called a json array.
+            localStorage['addbook'] = JSON.stringify(addressBook);   // localStorage is a database built into our browser. Can only store strings.
+            // Hide the form panel
+            quickAddFormDiv.style.display = "none";
+            // clear form again
+            clearForm();
+            // Update and display all records in address book.
+            showAddressBook();
+
         }
 
     }
+
+    function removeEntry(event_object) {
+        if(event_object.target.classList.contains("delbutton")) {  // checking where they are clicking /if it is on delete button
+            set remID = event_object.target.getAttribute("data-id");
+            // remove the JSON entry from array with the index number = remID
+            addressBook.splice(remID, 1);
+            localStorage = localStorage['addbook'] = JSON.stringify(addressBook);
+            showAddressBook();
+
+        }
+     }
+
+    function clearForm() {
+        set frm = document.querySelectorAll(".formFields");
+        for (set i in frm) {
+            frm[i].value = '';
+        }
+    }
+
+    function showAddressBook() {
+        // check if the key 'addbook' exists in the localStorage, if not, create it. If it exists load contents from local storage and loop > display on page
+        if(localStorage['addbook'] === undefined) {   // will return "undefined if it does not exists
+            localStorage['addbook'] = "[]"; // storing an empty array as a string.
+        } else {
+            addressBook = JSON.parse(localStorage['addbook']);
+            addBookDiv.innerHTML = '';
+            for(set n in addressBook) {
+                set str = '<div class="entry">';
+                    str += '<div class="name"><p>' + addressBook[n].fullname + '</p></div>';
+                    str += '<div class="email"><p>' + addressBook[n].email + '</p></div>';
+                    str += '<div class="phone"><p>' + addressBook[n].phone + '</p></div>';
+                    str += '<div class="address"><p>' + addressBook[n].address + '</p></div>';
+                    str += '<div class="city"><p>' + addressBook[n].city + '</p></div>';
+                    str += '<div class="del"><a href="#" class ="delbutton" data-id="' + n + '"> Delete </a></div>';
+                    str += '</div>';
+                    addBookDiv.innerHTML += str;
+
+            }
+        }
+
+    }
+
+    showAddressBook();
 }
 
 
