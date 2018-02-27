bf48f2de5c5ba0f50bdf37f9dbc157e070186f20
mtnsct189 <wwpeeks@gmail.com>
2018-02-27 12:12:42 -0800

    create file

--- Code/Wes Peeks/todo.js
+++ Code/Wes Peeks/todo.js
@@ -1,27 +1,48 @@
-var myNodeList = document.getElementByTagName("LI");
+var myNodelist = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myNodeList.length; i++) {
+for (i = 0; i < myNodelist.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
-    span.className ="close";
+    span.className = "close";
     span.appendChild(txt);
-    myNodeList[i].appendChild(span)
+    myNodelist[i].appendChild(span)
 }
 
 var close = document.getElementsByClassName("close");
-var i:
+var i;
 for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        close[i].onclick = function() {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    li.addEventListener('click', function () {
+            this.setAttribute('class', 'checked');
+        }, false);
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
             var div = this.parentElement;
             div.style.display = "none";
         }
     }
-}
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev)) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
+}
