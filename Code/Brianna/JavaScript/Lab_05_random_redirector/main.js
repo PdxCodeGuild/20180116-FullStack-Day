8b42d014269fb73eb784c1c8defff724887e2aa6
Brianna Harris <Brianna@BriannaHarris.net>
2018-02-27 16:14:59 -0800

    go to a random website - working.  Needs visual work, etc.

--- Code/Brianna/JavaScript/Lab_05_random_redirector/main.js
+++ Code/Brianna/JavaScript/Lab_05_random_redirector/main.js
@@ -1,5 +1,16 @@
 
 
+let website_array = ["https://www.booooooom.com/", "https://www.brainpickings.org/", "https://laughingsquid.com/", "http://www.theuselessweb.com/",
+"http://theoatmeal.com/", "http://explosm.net/", "https://hyperboleandahalf.blogspot.com/", "https://omgfacts.com/", "http://www.staggeringbeauty.com/", "http://paperjs.org/about/"];
+let arrayLength = website_array.length;
+alert(arrayLength);
+let random_index = Math.floor(Math.random() * arrayLength);
+let go_to_a_website_bt = document.querySelector('#go_to_a_website');
+
+alert(random_index);
+go_to_a_website_bt.onclick = function () {
+    location = website_array[random_index];
+};
 
 
 
@@ -8,6 +19,8 @@
 https://www.booooooom.com/
 
 https://www.brainpickings.org/
+
+ http://schillmania.com/projects/soundmanager2/
 
 https://laughingsquid.com/
 
