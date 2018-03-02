f348b074a014ad5886e08964c8184cad773d850b
michaelx <michaelvertex@gmail.com>
2018-03-01 17:04:50 -0800

    # day one

--- Code/Michael/JavaScript/lab5_randRedirect/js/lab5_randRedirect.js
+++ Code/Michael/JavaScript/lab5_randRedirect/js/lab5_randRedirect.js
@@ -4,8 +4,14 @@
 
 document.getElementById("clicked").onclick = function () {
 
-    let favoriteRecords = [
+    let recs = [
+    'https://www.discogs.com/Various-Blue-Skied-An-Clear-A-Morr-Music-Compilation/master/60921',
+    'https://www.discogs.com/Vikki-Jackman-Of-Beauty-Reminiscing/master/42075',
+    'https://www.discogs.com/Gary-Numan-The-Pleasure-Principle/master/70133',
+    'https://www.discogs.com/Section-25-Always-Now/master/55226',
     'https://www.discogs.com/COH-Above-Air/master/960452',
+    'https://www.discogs.com/Cindytalk-A-Life-Is-Everywhere/master/558435',
+    'https://www.discogs.com/Antlers-Mulm-The-Age-Of-Efficiency/master/337539',
     'https://www.discogs.com/Current-93-Thunder-Perfect-Mind/master/22268',
     'https://www.discogs.com/Alva-Noto-Xerrox-Vol2/master/173456',
     'https://www.discogs.com/Byetone-Symeta/master/391269',
@@ -21,6 +27,7 @@ document.getElementById("clicked").onclick = function () {
     'https://www.discogs.com/These-Immortal-Souls-Im-Never-Gonna-Die-Again-/master/213907',
     'https://www.discogs.com/Black-Marble-Its-Immaterial/master/1071230',
     'https://www.discogs.com/Brendan-Walls-Andrew-Chalk-This-Growing-Clearing/release/582278',
+    'https://www.discogs.com/The-Misfits-Static-Age/master/105468',
     'https://www.discogs.com/Aglaia-Mondi-Sensibili/release/803661',
     'https://www.discogs.com/Rowland-S-Howard-Pop-Crimes/master/216098',
     'https://www.discogs.com/Cylob-Cylobian-Sunset/release/82128',
@@ -29,10 +36,10 @@ document.getElementById("clicked").onclick = function () {
     'https://www.discogs.com/Suicidal-Tendencies-Suicidal-Tendencies/master/18713',
     'https://www.discogs.com/Cyclobe-Wounded-Galaxies-Tap-At-The-Window/master/289307',
     'https://www.discogs.com/Christian-Death-Only-Theatre-Of-Pain/master/2053',
-    'https://www.discogs.com/Current-93-With-The-Black-And-Red-Menstrual-Show-Christ-And-The-Pale-Queens-Mighty-In-Sorrow/master/21939',
+    'https://www.discogs.com/Black-Flag-The-First-Four-Years/master/28709',
     ];
 
-    let picked = Math.floor(Math.random() * favoriteRecords.length);
-    location.href = favoriteRecords[picked];
+    let picked = Math.floor(Math.random() * recs.length);
+    location.href = recs[picked];
 
     };
