af450a361c176d97534ad3b06c550728e062f48a
ninjabee <bkharris77@gmail.com>
2018-03-09 13:25:04 -0800

    Minor updates to version two of quote of the day.

--- Code/Brianna/JavaScript/lab_09_quote_of_the_day_testing_Jquery/ajax.js
+++ Code/Brianna/JavaScript/lab_09_quote_of_the_day_testing_Jquery/ajax.js
@@ -2,9 +2,8 @@
 $("#get_quote").on('click', function(){
         $.ajaxSetup({cache:false});
 
-        $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-        $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
-
+        $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=4&callback=", function(a) {
+            $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
             $(".message").html(data[0].content + "_" + data[0].title)
     });
 });
