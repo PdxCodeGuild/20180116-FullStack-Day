75aba8ca657e5474f6c21123a4a7652d41aa00b2
ninjabee <bkharris77@gmail.com>
2018-03-09 13:25:48 -0800

    Minor updates to version two of quote of the day.

--- Code/Brianna/JavaScript/lab_09_quote_of_the_day_v2/main.js
+++ Code/Brianna/JavaScript/lab_09_quote_of_the_day_v2/main.js
@@ -1,8 +1,11 @@
 let api_key = "c4ebc4f09dfaf7a2947a90250196a5a9";  // normally this would be hidden away somewhere. Since we do not care if sample random quotes revokes our access we are posting.
 let quote_block = document.getElementById('quote_block'); // Hook into my block quote element
-let page_number = document.querySelector('#page_button');
-let filter_by = document.querySelector('#filter_by');
 
+let page_number = document.querySelectorAll('.page_button');
+
+let filter_by = document.querySelector('#filter_by');
+let submit_button = document.querySelector('#submit_button');
+let key_word = '';
 
 // My first call to Ajax
 function http_get(url, success) {   // function to request data from API
@@ -10,13 +13,17 @@ function http_get(url, success) {   // function to request data from API
     xhttp.onreadystatechange = function() {
         if (this.readyState === 1) {
             xhttp.setRequestHeader('Authorization', 'Token token="'+ api_key + '"');
+            console.log(1)
         } else if (this.readyState === 4 && this.status === 200) {
             let data = JSON.parse(xhttp.responseText);
             console.log(data);
+            console.log(2)
             success(data);
+
         } else if (this.status !== 200) {
             let url = 'https://http.cat/' + this.status;    // add cute cat images for errors
-            document.querySelector('#cat_error_img').src = url;  // display cute cate images
+            document.querySelector('#cat_error_img').src = url;  // display cute cat images
+            console.log(3)
         }
     };
     xhttp.open("GET", url); // What action we want to take (ie: GET, POST, PUT, or DELETE)
@@ -25,54 +32,59 @@ function http_get(url, success) {   // function to request data from API
 
 console.log(document);
 
-// Build a function to get quotes and append them to the page.
-function get_quotes(key_word) {
-  let url = "https://favqs.com/api/quotes?page="+page_number+"&filter=" + text;
-  http_get(url, function(data) {
+// make button objects
+let left_button = document.querySelector('#page_button_1');
+let right_button = document.querySelector('#page_button_2');
+let current_page = 1;
 
-    filter_by.innerHTML = '';
-    quote_block.innerHTML = '';
-
-    let total_pages = data.total_pages;
-    for (let i=0; i<total_pages; ++i) {
-      let page_button = document.createElement('button');
-      page_bt.innerText = (i+1);
-      page_bt.onclick = function() {
-        get_users(i+1);
-      };
-      filter_by.appendChild(page_button);
+// get page number value
+left_button.onclick = function () {
+    if (current_page > 1) {
+        curent_page -= 1;
     }
-    let quote = data.quote.body;
-    for (let i=0; i<quote.length; ++i) {
-
-      quote_block.innerText = data.quote.body + ' ~ ' + data.quote.author
-
-      let img = document.createElement('img');
-      img.src = img_url;
-
-      let user_div = document.createElement('div');
-      user_div.appendChild(quote);
-
-      users_div.appendChild(user_div);
-
-    }
+    http_get("https://favqs.com/api/quotes?page="+current_page+"&filter=" + filter_by, function (data) {
+        console.log(data);
+        quote_block.innerText = data.quote.body + ' ~ ' + data.quote.author;
   });
+};
 
-//
-if (page_number === 2 || page_number === 3 || page_number === 4 || page_number === 5) {
-    let url = 'https://favqs.com/api/quotes/?filter=&type=tag&page=' + page_number.value;
-} else {
-    let url = 'https://favqs.com/api/quotes/?filter=&type=tag&page=1'
-}
+right_button.onclick = function () {
+    current_page += 1;
+    http_get("https://favqs.com/api/quotes?page="+current_page+"&filter=" + filter_by.value, function (data) {
+
+        console.log(data);
+        quote_block.innerText = data.quote.body + ' ~ ' + data.quote.author;
+  });
+};
+
+// get submit value
+
+submit_button.onclick = function () {
+    current_page = 1;
+    key_word = filter_by.value;
+    console.log("hey")
+    http_get("https://favqs.com/api/quotes?page="+current_page+"&filter=" + filter_by.value, function (data) {
+
+        console.log(data);
+        quote_block.innerText = data.quote.body + ' ~ ' + data.quote.author;
+  });
+};
+
+
 
 
 // let url = 'https://favqs.com/api/quotes/?filter=' + search_term.value + '&type=tag&page=' + page_number.value;
 
-http_get(url, function (data) {
 
-    console.log(data);
-    quote_block.innerText = data.quote.body + ' ~ ' + data.quote.author;
-});
+// http_get(url, function (data) {
+//
+//     console.log(data);
+//     quote_block.innerText = data.quote.body + ' ~ ' + data.quote.author;
+// });
+
+// append multiple quotes to the page
+// show and hide “pages” of content when you press a button
+// keep track of what quotes are in what page
 
 
 
