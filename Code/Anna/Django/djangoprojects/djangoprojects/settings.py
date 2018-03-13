af7de7fd05ebf4c6186b224951b32533fde50fa7
Anna <anna@annaspysz.com>
2018-03-13 16:05:41 -0700

    More setup stuff

--- Code/Anna/Django/djangoprojects/djangoprojects/settings.py
+++ Code/Anna/Django/djangoprojects/djangoprojects/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lab02_todo.apps.Lab02TodoConfig',
+    'lab01_polls.apps.Lab01PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
 
