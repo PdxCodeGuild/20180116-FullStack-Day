689a030c549ad8632c90bd40e7cd2ba73fdbdcad
ninjabee <bkharris77@gmail.com>
2018-03-13 16:09:07 -0700

    Starting work on views

--- Code/Brianna/todoapp/todo/views.py
+++ Code/Brianna/todoapp/todo/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Todo
 
 def index(request):
-    return HttpResponse("Hello my darling to do list! How are you?")
+    latest_todo_list = Todo.objects.order_by('-pub_date')[:5]
+    output = ', '.join([t.question_text for t in latest_todo_list])
+    return HttpResponse(output)
 
+def detail(request, todo_id):
+    return HttpResponse("You're looking at our todo list." % question_id)
+
+def results(request, todo_id):
+    response = "You're looking at our completed items in our todo list."
+    return HttpResponse(response % question_id)
+
+def complete(request, todo_id):
+    return HttpResponse("You're completing a todo item." % question_id)
 
 
 
