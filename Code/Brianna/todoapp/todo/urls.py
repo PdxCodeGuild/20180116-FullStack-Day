6cb637c048c382e4351883b18d9039219a69789f
ninjabee <bkharris77@gmail.com>
2018-03-14 16:25:44 -0700

    Working on Django todo list. Currently can add list items via an input field.

--- Code/Brianna/todoapp/todo/urls.py
+++ Code/Brianna/todoapp/todo/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 
-
+app_name = "todo"
 urlpatterns = [
     path('', views.index, name='index'),
     path('int:todo_id>/', views.results, name='results'),
     path('<int:todo_id>/results/', views.results, name='results'),
     path('<int:todo_id>/complete/', views.complete, name='complete'),
+    path('newtodo/', views.newtodo, name='newtodo'),
 ]
 
