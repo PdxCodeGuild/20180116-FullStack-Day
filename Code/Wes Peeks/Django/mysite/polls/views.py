3ac87b981c257df2d5418fa4010bd115c3ecdd00
mtnsct189 <wwpeeks@gmail.com>
2018-03-16 08:59:38 -0700

    Django Lab1

--- Code/Wes Peeks/Django/mysite/polls/views.py
+++ Code/Wes Peeks/Django/mysite/polls/views.py
@@ -4,24 +4,33 @@ from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.urls import reverse
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+def IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+def detail(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+def results(generic.DetailView):
+   model = Question
+   template_name = 'polls/detail.html'
 
 def vote(request, question_id):
-    #question = get_object_or_404(Question, pk=question_id)
-    choice_id = request.POST['choice']
-    choice = Choice.objects.get(pk=choice_id)
-    choice.votes += 1
-    choice.save()
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (keyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question': question,
+            'error_message': "You didn't slect a choice.",
+        })
+    else:
+    selected_choice.votes += 1
+    selected_choice.save()
 
     return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
