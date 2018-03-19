1053e52251f8c01ed6cc7e7e005d318064183a63
Maggie Geise <maggie.geise@gmail.com>
2018-03-19 11:19:21 -0700

    todolist

--- Code/Maggie/django/lab_02/lab_02/urls.py
+++ Code/Maggie/django/lab_02/lab_02/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# from django.urls import path
-from django.conf.urls import url, include
+from django.urls import path, include
+
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'^todo_list/', include('todo_list.urls')),
+    path('admin/', admin.site.urls),
+    path('todo_list/', include('todo_list.urls')),
 ]
