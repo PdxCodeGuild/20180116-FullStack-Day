6db4bd3a569d6e0fe276374e2fe456c591ce8dac
Brianna Harris <Brianna@BriannaHarris.net>
2018-03-19 19:20:27 -0700

    Updating files

--- Code/Brianna/mysite/shortener/models.py
+++ Code/Brianna/mysite/shortener/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Shortener(models.Model):
+
+    # Create a url field to input a new url to be shortened
+    input_url_field = models.CharField(max_length=200)
+
+
+    def shorten(self):
+        # Create a function to shorten urls
+        return HttpResponse('hello!')
+
+
+    def __str__(self):
+      return self.shortener.input_url_field + 'Hello!'
+
+
--- Code/Brianna/mysite/shortener/views.py
+++ Code/Brianna/mysite/shortener/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+
+
+from django.shortcuts import get_object_or_404, render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Shortener
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Boo index!')
+
+def base(request):
+    return HttpResponse('Boo base!')
+
+def receive(request):
+    return HttpResponse('Boo Receive')
+
+def redirect(request):
+    return HttpResponse('Boo Response')
+
+
