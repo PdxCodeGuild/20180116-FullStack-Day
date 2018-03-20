aad4f25d013caff1b13cdde4566fc7d532c73603
Brianna Harris <Brianna@BriannaHarris.net>
2018-03-20 10:31:00 -0700

    Updating files

--- Code/Brianna/mysite/shortener/views.py
+++ Code/Brianna/mysite/shortener/views.py
@@ -4,19 +4,34 @@ from django.shortcuts import get_object_or_404, render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Shortener
 
-# Create your views here.
 
 def index(request):
-
-    return HttpResponse('Boo index!')
-
-def base(request):
-    return HttpResponse('Boo base!')
+    long_url = Shortener.objects.all()
+    context = {'long_url': long_url}
+    return render(request, 'shortener/index.html', context)
 
 def receive(request):
-    return HttpResponse('Boo Receive')
+    url = get_object_or_404(Shortener, pk=url_id )
+
+
+    context = {'url': url}, {'short_url':short_url}, {'url_id':url_id}
+    return render(request, 'shortener/index.html', context)
 
 def redirect(request):
     return HttpResponse('Boo Response')
+
+
+def saveurl(request):
+    url_id = request.POST['url_id']
+    short_url = request.POST['short_url']
+    long_url = request.POST['long_url']
+    url = Shortener.objects.get(pk=url_id)
+    url.short = short_url
+    url.long = long_url
+    url.save()
+    return HttpResponseRedirect(reverse('shortener:index'))
+
+def create_short_url(request):
+    return HttpResponseRedirect(reverse('shortener:index'))
 
 
