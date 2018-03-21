66060a03675124df9fdf8d14c9a910999e51bf22
Brianna Harris <Brianna@BriannaHarris.net>
2018-03-21 16:47:41 -0700

    making new project for shortener

--- Code/Brianna/shortener/shorturl/views.py
+++ Code/Brianna/shortener/shorturl/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('ok')
+
