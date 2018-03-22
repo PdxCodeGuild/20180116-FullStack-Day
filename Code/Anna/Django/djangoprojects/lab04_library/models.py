58417df8d272af11d55f902dc2cc9e7252b4f84a
Anna Spysz <anna@annaspysz.com>
2018-03-22 16:21:24 -0700

    Working version 1 of library lab

--- Code/Anna/Django/djangoprojects/lab04_library/models.py
+++ Code/Anna/Django/djangoprojects/lab04_library/models.py
@@ -15,9 +15,9 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)  # fk because author can have multiple books
     pub_date = models.DateTimeField()  # set once
     checked_out = models.BooleanField(default=False)  # changed when user checks out
-    who_checked_out = models.ForeignKey(User, on_delete=models.SET_NULL)  # records user who checks out at the time
-    timestamp_out = models.DateTimeField()  # when checked out
-    timestamp_in = models.DateTimeField()  # when checked back in
+    who_checked_out = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  # records user who checks out at the time
+    timestamp_out = models.DateTimeField(null=True, blank=True)  # when checked out
+    timestamp_in = models.DateTimeField(null=True, blank=True)  # when checked back in
 
     def __str__(self):
         return self.title
