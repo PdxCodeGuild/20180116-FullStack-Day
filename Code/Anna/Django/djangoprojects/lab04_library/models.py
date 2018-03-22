2b9d725fd13222e9cc78d094249844e18e6d4454
Anna Spysz <anna@annaspysz.com>
2018-03-22 15:01:30 -0700

    Setting up Library app with users

--- Code/Anna/Django/djangoprojects/lab04_library/models.py
+++ Code/Anna/Django/djangoprojects/lab04_library/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+# make migrations whenever models are changed - migrations update the database!
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField()
+    checked_out = models.BooleanField(default=False)
+
+    def checked_out(self):
+        return self.checked_out is True
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    user = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user
+
+# book: the book that the user checked out or checked in
+# user: a text field containing the name of the user that checked out or checked in the book
+# checkout: a boolean indicating whether the book was checked out or checked in
+# timestamp: a datetime that records when the book was checked out or in
--- Code/Anna/Django/djangoprojects/lab04_library/views.py
+++ Code/Anna/Django/djangoprojects/lab04_library/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.http import HttpResponseRedirect
+
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.models import User, Group, Permission
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'lab04_library/index.html', context)
