9b5973dee3bdf9ca17a70bfb51fec5711ca72a6d
mtnsct189 <wwpeeks@gmail.com>
2018-03-23 10:00:47 -0700

    Django Lab1

--- Code/Wes Peeks/Django/todolist/todolist/settings.py
+++ Code/Wes Peeks/Django/todolist/todolist/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'uriel.apps.UrielConfig',
     'todos.apps.TodosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
