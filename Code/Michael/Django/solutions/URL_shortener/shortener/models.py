4f04a6f63a661c131b5b8da0043f5786c65ed383
michaelx <michaelvertex@gmail.com>
2018-03-22 21:23:03 -0700

    # day one

--- Code/Michael/Django/solutions/URL_shortener/shortener/models.py
+++ Code/Michael/Django/solutions/URL_shortener/shortener/models.py
@@ -1,12 +1,18 @@
+
 from django.db import models
 
 # Create your models here.
+from .utils import code_generator
 
 class ShortenerURL(models.Model):
     url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15, unique=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.shortcode = code_generator()
+        super(ShortenerURL, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
