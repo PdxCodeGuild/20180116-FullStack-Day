0dc57f48b6db35d489df52c404d2d2c04cb84878
mtnsct189 <wwpeeks@gmail.com>
2018-03-26 08:40:21 -0700

    Django Lab1

--- Code/Wes Peeks/python/lab10.py
+++ Code/Wes Peeks/python/lab10.py
@@ -1,11 +1,17 @@
-nums = []
+numbers = [0 , 4, 6, 7, 3, 5, 7, 8,]
+sums = sum(numbers)
+average = sums / len(numbers)
+print(average)
 
-user_num = input('What number would you like to add?\n:')
-user_num = num.append()
 
-for num in nums:
-    print(num)
-
-for i in range(len(nums)):
-    print(nums[i])
+# nums = []
+#
+# user_num = input('What number would you like to add?\n:')
+# user_num = num.append()
+#
+# for num in nums:
+#     print(num)
+#
+# for i in range(len(nums)):
+#     print(nums[i])
 
