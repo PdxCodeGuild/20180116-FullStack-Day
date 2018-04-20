29855c102de1b41da0da202c755554fd0d4c4ce1
michaelx <michaelvertex@gmail.com>
2018-04-19 21:40:41 -0700

    # day one

--- Code/Michael/JavaScript/lab9_randQ/js/lab9_randQ.js
+++ Code/Michael/JavaScript/lab9_randQ/js/lab9_randQ.js
@@ -19,11 +19,10 @@ function http_get(url, success) {
     xhttp.send();
 }
 
-
 function get_data() {
 
     http_get('https://favqs.com/api/quotes/', function(data) {
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 1; i++) {
             let node = document.createElement("li");
             node.innerText = data.quotes[i].body + ' - ' + data.quotes[i].author;
             bq.appendChild(node);
@@ -31,13 +30,9 @@ function get_data() {
     });
 }
 
-
 setInterval(get_data, 10000);
 
 
 clicked.addEventListener("click", function() {
     get_data();
 });
-
-
-
