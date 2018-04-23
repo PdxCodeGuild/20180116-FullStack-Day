4b8fe6bb9058c13abddb4780fae47bd75079c8d2
Sergio <sergiobluez@gmail.com>
2018-04-22 23:06:23 -0700

    fixed

--- Code/Sergio/Python/Lab25-ATM.py
+++ Code/Sergio/Python/Lab25-ATM.py
@@ -1,15 +1,6 @@
-"""Lab 25: ATM"""
-
-"""Let's represent an ATM with a class containing two attributes: a balance and an interest rate. A newly created account will default to a balance of 0 and an interest rate of 0.1%. Implement the initializer, as well as the following functions:
-
-    check_balance() returns the account balance
-    deposit(amount) deposits the given amount in the account
-    check_withdrawal(amount) returns true if the withdrawn amount won't put the account in the negative
-    withdraw(amount) withdraws the amount from the account and returns it
-    calc_interest() returns the amount of interest calculated on the account
 """
-
-"""Lab 25 : ATM"""
+Lab 25 - ATM
+"""
 
 
 # ATM class with all functions
@@ -33,11 +24,10 @@ class ATM:
         else:
             print('Not enough funds to withdraw.')
 
-    def withdraw(self, amount):  # *withdraws $$$$$$$$*
-        if self.balance - amount > 0:
-            return f"You have withdrawn ${amount}"
-        else:
-            print('Not enough funds to withdraw.')
+    def withdraw(self, amount):  # deposits to balance
+        self.balance -= amount
+        print(f"You have withdrawn: {amount}")
+        return self.balance
 
     def calc_interest(self):
         self.balance += self.balance * self.interest
@@ -51,10 +41,11 @@ class ATM:
 # testing
 checking_account = ATM(50000)
 print(checking_account.check_balance())
+print(checking_account.check_withdrawal(900))
 print(checking_account.withdraw(900))
 print(checking_account.check_balance())
 print(checking_account.deposit(int(1000)))
 print(checking_account.check_balance())
-print(checking_account.withdraw(1500))  # change to ridiculously high nunber to get else statement
+print(checking_account.withdraw(1500))  # change to ridiculously high number to get else statement
 print(checking_account.deposit(2500))
 print(checking_account.calc_interest())
