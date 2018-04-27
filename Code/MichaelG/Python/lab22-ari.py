55cda03c841ef3a766e43003714f23d25f0fa2d0
MTGood86 <mtgood86@gmail.com>
2018-04-26 22:59:54 -0700

    Finished Lab 21 count words

---
+++ Code/MichaelG/Python/lab22-ari.py
@@ -0,0 +1,9 @@
+file = open("guten_lab.txt", 'r+')
+wordcount = {}
+for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+for k, v in wordcount.items():
+    print(k, v)
